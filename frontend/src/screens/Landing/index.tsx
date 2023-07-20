0845cf2c3ae67d0edfe7e7bce1293012f2109837
jazzgrewal <jaski.grewal@gmail.com>
2023-07-19 19:18:19 -0700

    Added the warnings

--- frontend/src/screens/Landing/index.tsx
+++ frontend/src/screens/Landing/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import BCGovLogo from "../../components/BCGovLogo";
-import { Button } from "@carbon/react";
+import { Button, Toggle, ToastNotification, InlineNotification } from "@carbon/react";
 import { Login } from '@carbon/icons-react';
 import { signIn } from "../../services/AuthService";
 import './Landing.scss';
@@ -14,8 +14,49 @@ const Landing: React.FC = () => {
         <div className="container-fluid">
           <div className="row">
             <div className="col-lg-7 px-4">
-            
-              <BCGovLogo />
+              <div className="d-flex flex-row justify-content-between align-items-center mt-4">
+                <BCGovLogo />
+                <Toggle
+                  size="md"
+                  labelA="Off"
+                  labelB="On"
+                  defaultToggled
+                  hideLabel
+                  id="toggle-1"
+                />
+              </div>
+
+              <div className="mt-3 ">
+                <InlineNotification
+                  iconDescription="describes the close button"
+                  subtitle={<span>See the release notes to find out what's new</span>}
+                  title={<span className="fw-bold">New Release!</span>}
+                  kind="info"
+                  lowContrast
+                  className = "w-100"
+                />
+              </div>
+              <div className="mt-3 ">
+                <InlineNotification
+                  iconDescription="describes the close button"
+                  subtitle={<span>SILVA will be offline for up to 4 hours time starting on June 30, 2023 at 9:00pm</span>}
+                  title={<span className="fw-bold">Upcoming maintenance</span>}
+                  kind="warning"
+                  lowContrast
+                  className = "w-100"
+                />
+              </div>
+              <div className="mt-3 ">
+                <InlineNotification
+                  iconDescription="describes the close button"
+                  subtitle={<span>SILVA is expected to come back online on July 1, 2023 at 11:11am</span>}
+                  title={<span className="fw-bold">SILVA is down</span>}
+                  kind="error"
+                  lowContrast
+                  className = "w-100"
+                />
+              </div>
+
               {/* Welcome - Title and Subtitle */}
               <h1 data-testid="landing-title" className="landing-title">Welcome to SILVA</h1>
               <h2 data-testid="landing-subtitle" className="landing-subtitle">
