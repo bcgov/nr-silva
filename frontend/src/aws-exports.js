442b41d2a49208e8473f8217de9129d388033538
Derek Roberts <derek.roberts@gmail.com>
2023-08-23 16:49:06 -0700

    Add REACT_APP_URL for redirect

--- frontend/src/aws-exports.js
+++ frontend/src/aws-exports.js
@@ -1,5 +1,7 @@
 import { env } from './env';
 
+const redirect_url = env.REACT_APP_URL || "http://localhost:3000";
+    
 const awsconfig = {
     aws_cognito_region: env.REACT_APP_COGNITO_REGION,
     aws_user_pools_id: env.REACT_APP_USER_POOLS_ID,
@@ -8,8 +10,8 @@ const awsconfig = {
     oauth: {
         domain: env.REACT_APP_AWS_DOMAIN,
         scope: ['openid'],
-        redirectSignIn: "http://localhost:3000/dashboard",
-        redirectSignOut: "http://localhost:3000/",
+        redirectSignIn: `${redirect_url}/dashboard`,
+        redirectSignOut: `${redirect_url}`,
         responseType: 'code',
     },
     federationTarget: 'COGNITO_USER_POOLS',
