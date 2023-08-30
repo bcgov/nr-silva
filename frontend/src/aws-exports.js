6949307162ae8d8b722945189af533a1ce286da3
jazzgrewal <jaski.grewal@gmail.com>
2023-08-30 12:17:45 -0700

    shorten the URL with variables

--- frontend/src/aws-exports.js
+++ frontend/src/aws-exports.js
@@ -1,5 +1,10 @@
 import { env } from './env';
 
+const ZONE = env.REACT_APP_ZONE.toLocaleLowerCase();
+const retUrl = window.location.origin;
+const logoutDomain = `https://logon${ZONE === "prod"?'':'test'}7.gov.bc.ca/`;
+
+
 const awsconfig = {
     aws_cognito_region: env.REACT_APP_COGNITO_REGION || "ca-central-1",
     aws_user_pools_id: env.REACT_APP_USER_POOLS_ID,
@@ -9,7 +14,7 @@ const awsconfig = {
         domain: env.REACT_APP_AWS_DOMAIN || "prod-fam-user-pool-domain.auth.ca-central-1.amazoncognito.com",
         scope: ['openid'],
         redirectSignIn: `${window.location.origin}/dashboard`,
-        redirectSignOut: env.REACT_APP_REDIRECT_SIGN_OUT || `https://logon${env.REACT_APP_ZONE.toLocaleLowerCase() === "prod"?'':'test'}7.gov.bc.ca/clp-cgi/logoff.cgi?retnow=1&returl=https://${env.REACT_APP_ZONE.toLocaleLowerCase()}.loginproxy.gov.bc.ca/auth/realms/standard/protocol/openid-connect/logout?redirect_uri=${window.location.origin}/`,
+        redirectSignOut: env.REACT_APP_REDIRECT_SIGN_OUT || `${logoutDomain}/clp-cgi/logoff.cgi?retnow=1&returl=https://${ZONE}.loginproxy.gov.bc.ca/auth/realms/standard/protocol/openid-connect/logout?redirect_uri=${retUrl}/`,
         responseType: 'code',
     },
     federationTarget: 'COGNITO_USER_POOLS',
