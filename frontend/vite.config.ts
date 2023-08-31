4e51089165c04a86be9556b763f0ea89e699cf64
Ricardo Campos <ricardo.campos@encora.com>
2023-08-31 17:00:59 -0300

    feat: add build config for vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,55 +1,61 @@
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  define: {
-    global: {}
-  },
-  plugins: [
-    {
-      name: 'build-html',
-      apply: 'build',
-      transformIndexHtml: (html) => {
-        return {
-          html,
-          tags: [
-            {
-              tag: 'script',
-              attrs: {
-                src: '/env.js'
-              },
-              injectTo: 'head'
-            }
-          ]
+export default defineConfig(({ mode }) => {
+  const config = {
+    define: {} as any,
+    plugins: [
+      {
+        name: 'build-html',
+        apply: 'build',
+        transformIndexHtml: (html) => {
+          return {
+            html,
+            tags: [
+              {
+                tag: 'script',
+                attrs: {
+                  src: '/env.js'
+                },
+                injectTo: 'head'
+              }
+            ]
+          }
         }
+      },
+      react()
+    ],
+    build: {
+      outDir: 'build'
+    },
+    server: {
+      port: 3000
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
       }
     },
-    react()
-  ],
-  build: {
-    outDir: 'build'
-  },
-  server: {
-    port: 3000
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-    css: true,
-    reporters: ['verbose'],
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'json', 'html'],
-      include: ['src/**/*'],
-      exclude: [],
-    }
-  },
-})
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/setupTests.ts',
+      css: true,
+      reporters: ['verbose'],
+      coverage: {
+        provider: 'v8',
+        reporter: ['text', 'json', 'html'],
+        include: ['src/**/*'],
+        exclude: [],
+      }
+    },
+  };
+
+  if (mode === 'development') {
+    config.define.global = {};
+  }
+
+  return config;
+});
