70ad032b031179ede8c8fdf325fefe6dfb6383dd
Jazz Grewal <39718912+jazzgrewal@users.noreply.github.com>
2023-08-31 14:30:16 -0700

    Added the User Profile sidebar with test data and cleaned up the Dashboard (#78)

---
+++ frontend/src/components/AvatarImage/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import emptyUser from '../../assets/img/emptyUser.jpg';
+import './AvatarImage.scss';
+
+type Size = 'small' | 'large';
+
+interface AvatarImageProps {
+  userName: string;
+  source?: string;
+  size: Size;
+}
+
+const AvatarImage = ({ userName, source, size }: AvatarImageProps) => (
+  <img src={source || emptyUser} alt={`${userName}`} className={`profile-image ${size}`} />
+);
+
+export default AvatarImage;
--- frontend/src/components/BCHeaderwSide/index.tsx
+++ frontend/src/components/BCHeaderwSide/index.tsx
@@ -21,6 +21,7 @@ import './BCHeaderwSide.scss';
 import RightPanelTitle from '../RightPanelTitle';
 import { env } from '../../env';
 import ThemeToggle from '../ThemeToggle';
+import MyProfile from '../MyProfile';
 
 interface ListItem {
   name: string;
@@ -43,60 +44,6 @@ const listItems = [
         link: '/dashboard',
         disabled: false
       },
-      {
-        name: 'Seedlots',
-        icon: 'SoilMoistureField',
-        link: '/seedlot',
-        disabled: true
-      },
-      {
-        name: 'Seedlings',
-        icon: 'CropGrowth',
-        link: '#',
-        disabled: true
-      },
-      {
-        name: 'Nurseries',
-        icon: 'CropHealth',
-        link: '#',
-        disabled: true
-      },
-      {
-        name: 'Orchards',
-        icon: 'MapBoundaryVegetation',
-        link: '#',
-        disabled: true
-      },
-      {
-        name: 'Reports',
-        icon: 'Report',
-        link: '/reports',
-        disabled: true
-      },
-      {
-        name: 'Tests',
-        icon: 'Chemistry',
-        link: '#',
-        disabled: true
-      },
-      {
-        name: 'Parent tree',
-        icon: 'Tree',
-        link: '#',
-        disabled: true
-      },
-      {
-        name: 'Tree seed center',
-        icon: 'Enterprise',
-        link: '#',
-        disabled: true
-      },
-      {
-        name: 'Financial',
-        icon: 'Money',
-        link: '#',
-        disabled: true
-      }
     ]
   },
   {
@@ -211,7 +158,7 @@ const BCHeaderwSide = () => {
               title="My Profile"
               closeFn={closeMyProfilePanel}
             />
-            <Button kind='secondary' onClick={()=>console.log('Hi Jazz')} >Log Out</Button>
+            <MyProfile/>
           </HeaderPanel>
           <SideNav isChildOfHeader expanded={isSideNavExpanded} aria-label="Side menu" className="bcheaderwside-sidenav">
             <SideNavItems>
