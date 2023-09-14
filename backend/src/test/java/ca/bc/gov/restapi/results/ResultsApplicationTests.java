d822b36b6523bec3ad46f30162e5355f561cc9ca
Ricardo Campos <ricardo.campos@encora.com>
2023-09-14 11:50:19 -0300

    feat: add backend initial files
    Issue #91
    
    This commit adds initial files generated by start.spring.io website
    where the project was generated with:
    
    - Project: Maven
    - Language: Java
    - Spring Boot version: 3.1.3 (latest stable)
    - Project metadata group: ca.bc.gov.restapi
    - Project metadata artifact: results
    - Project metadata name: results
    - Project metadata description: RESULTS REST API
    - Packaging: Jar
    - Java version: 17
    - Dependencies
      - Actuator
      - Data JPA
      - OAuth2 Authorization server
      - OAuth2 client
      - Started web
      - Devtools
      - H2 Database
      - Lombok
      - GraalVM

---
+++ backend/src/test/java/ca/bc/gov/restapi/results/ResultsApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.bc.gov.restapi.results;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResultsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
