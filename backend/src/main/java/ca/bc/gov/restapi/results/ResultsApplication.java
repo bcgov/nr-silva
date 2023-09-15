f837737291af78c19b16d60930f074435760175c
Ricardo Campos <ricardo.campos@encora.com>
2023-09-15 16:47:36 -0300

    feat: add google checkstyle
    Issue #91
    
    This commit adds the google checkstyle xml definition file to be used as
    a guide for the checkings. Also updates existing class files to met the
    checkstyle validations, replacing tab by spaces, adding JavaDoc to all
    public classes.

--- backend/src/main/java/ca/bc/gov/restapi/results/ResultsApplication.java
+++ backend/src/main/java/ca/bc/gov/restapi/results/ResultsApplication.java
@@ -3,11 +3,11 @@ package ca.bc.gov.restapi.results;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/** Entry point for the RESULTS REST API application. */
 @SpringBootApplication
 public class ResultsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ResultsApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ResultsApplication.class, args);
+  }
 }
