69bf7a201bd4aa7eb90b68428d7518b2ea65c243
jazzgrewal <jaski.grewal@gmail.com>
2023-12-08 18:52:40 -0800

    added the Map in the OpeningScreen

---
+++ frontend/src/components/OpeningsMap/index.tsx
@@ -0,0 +1,33 @@
+import React, { useRef } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+
+interface MapProps {
+  selectedBasemap: any;
+}
+
+const OpeningsMap: React.FC<MapProps> = ({ selectedBasemap }) => {
+  const position: [number, number] = [49.11941257871176, -122.83461402566606];
+  const lastClickedLayerRef = useRef<any>(null); // Replace 'any' with the specific type if known
+
+  return (
+    <MapContainer
+      center={position}
+      zoom={13}
+      scrollWheelZoom={false}
+      style={{ height: "400px", width: "100%" }}
+    >
+      <TileLayer
+        attribution={selectedBasemap.attribution}
+        url={selectedBasemap.url}
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable. This is a fixed Marker on Map load
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default OpeningsMap;
--- frontend/src/screens/Opening/index.tsx
+++ frontend/src/screens/Opening/index.tsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import { useSelector } from "react-redux";
+import { Button } from "@carbon/react";
 import StandardCard from "../../components/StandardCard";
 import FavouriteCard from "../../components/FavouriteCard";
 import PageTitle from "../../components/PageTitle";
 import RecentOpeningsTable from "../../components/RecentOpeningsTable";
 import { recentOpeningItems, recentOpeningItems1, recentOpeningsHeader } from "../../mock-data/constants";
+import './Opening.scss'
+import { ViewFilled } from '@carbon/icons-react';
+import OpeningsMap from "../../components/OpeningsMap";
 
 
 const Opening: React.FC = () => {
+  const [showSpatial, setShowSpatial] = useState<boolean>(false);
+
+  const toggleSpatial = () => {
+    setShowSpatial(!showSpatial)
+  }
+
   const userDetails = useSelector((state:any) => state.userDetails)
   const goToActivity = () => {
     console.log("clicked a row")
@@ -41,8 +51,24 @@ const Opening: React.FC = () => {
       <div className="container-fluid">
         <div className="row p-4">
             <PageTitle title="Recent Openings" subtitle="Track your recent openings and select to check spatial activity" />
+            <Button className="h-100 my-auto" renderIcon={ViewFilled} type="button" onClick={toggleSpatial}>
+              {showSpatial?'Hide Spatial':'Show Spatial'}
+            </Button>
         </div>
+        {showSpatial?(
+          <div className="row px-2">
+            <div className="leaflet-container">
+              <OpeningsMap selectedBasemap={{
+                id: 1,
+                name: "Google Maps Satelite",
+                attribution: '&copy; Google Maps',
+                url: "https://www.google.cn/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}",
+              }}/>
+            </div>
+          </div>
+        ):null}
       </div>
+      
 
       <div className="container-fluid">
         <RecentOpeningsTable
