8c932876d2a36619c9c24bbe4c4f87a986c380bc
Jazz Grewal <39718912+jazzgrewal@users.noreply.github.com>
2024-01-09 16:48:23 -0800

    changed the caddy config and declared the leaflet in modules (#186)
    * changed the caddy config and declared the leaflet in modules
    
    * changed the caddy config and declared the leaflet in modules
    
    * Adjust Caddyfile
    
    * added the search functionality inthe Opening screen table
    
    * Adjust Caddyfile
    
    ---------
    
    Co-authored-by: Derek Roberts <derek.roberts@gmail.com>

--- frontend/src/components/RecentOpeningsTable/index.tsx
+++ frontend/src/components/RecentOpeningsTable/index.tsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { hashObject } from 'react-hash-string';
-import { Table, TableHead, TableHeader, TableRow, TableBody, TableCell, Button, TableToolbar, TableToolbarSearch, TableToolbarContent, TableToolbarMenu, TableToolbarAction } from '@carbon/react';
+import {
+  Table,
+  TableHead,
+  TableHeader,
+  TableRow,
+  TableBody,
+  TableCell,
+  Button,
+  TableToolbar,
+  TableToolbarSearch,
+  TableToolbarContent,
+} from '@carbon/react';
 import * as Icons from '@carbon/icons-react';
 import StatusTag from '../StatusTag';
 import EmptySection from '../EmptySection';
@@ -14,12 +25,14 @@ interface TableProps {
 }
 
 const RecentOpeningsTable = ({ elements, headers, clickFn }: TableProps) => {
+  const [filteredElements, setFilteredElements] = useState(elements);
+
   const iconSize = '18';
 
   const createTableCell = (obj: any, key: string, index: number) => {
     const mapKey = `${key}-${index}`;
     let cellContent;
-  
+
     switch (key) {
       case 'status':
         cellContent = <StatusTag type={obj[key]} />;
@@ -29,7 +42,7 @@ const RecentOpeningsTable = ({ elements, headers, clickFn }: TableProps) => {
         cellContent = obj[key];
         break;
     }
-  
+
     return (
       <TableCell key={mapKey} className="activities-table-cell">
         {cellContent}
@@ -37,18 +50,32 @@ const RecentOpeningsTable = ({ elements, headers, clickFn }: TableProps) => {
     );
   };
 
+  const handleSearchChange = (searchTerm: string) => {
+    const filtered = elements.filter((item) =>
+      Object.values(item)
+        .join(' ')
+        .toLowerCase()
+        .includes(searchTerm.toLowerCase())
+    );
+    setFilteredElements(filtered);
+  };
+
   return (
     <>
       <TableContainer>
-      <TableToolbar aria-label="data table toolbar">
+        <TableToolbar aria-label="data table toolbar">
           <TableToolbarContent>
-            <TableToolbarSearch onChange={()=>{console.log("search term changed")}} placeholder={"Filter by opening ID, File ID, timber mark, cut block, status..."} persistent />
+            <TableToolbarSearch
+              onChange={(e:any) => handleSearchChange(e.target.value)}
+              placeholder="Filter by opening ID, File ID, timber mark, cut block, status..."
+              persistent
+            />
             <Button
               hasIconOnly
               iconDescription="Download"
               tooltipPosition="bottom"
               kind="ghost"
-              onClick={() => console.log("Download Click")}
+              onClick={() => console.log('Download Click')}
               renderIcon={Icons.Download}
               size="md"
             />
@@ -57,7 +84,7 @@ const RecentOpeningsTable = ({ elements, headers, clickFn }: TableProps) => {
               iconDescription="Print"
               tooltipPosition="bottom"
               kind="ghost"
-              onClick={() => console.log("Print Click")}
+              onClick={() => console.log('Print Click')}
               renderIcon={Icons.Printer}
               size="md"
             />
@@ -74,47 +101,51 @@ const RecentOpeningsTable = ({ elements, headers, clickFn }: TableProps) => {
             </TableRow>
           </TableHead>
           {/* Check if the table has some elements */}
-          {elements.length>0?(
+          {filteredElements.length > 0 ? (
             <TableBody aria-live="off">
-            {elements.map((item, idx) => (
-              <TableRow key={hashObject(item)} id={`row${idx}`}>
-                {Object.keys(item).map((key) => createTableCell(item, key, idx))}
-                <TableCell className="activities-table-action" tabIndex={0} aria-label="View more">
-                  <Button
-                    hasIconOnly
-                    iconDescription="View"
-                    tooltipPosition="bottom"
-                    kind="ghost"
-                    onClick={() => clickFn(item.id)}
-                    renderIcon={Icons.DataViewAlt}
-                    size="md"
-                  />
-                  <Button
-                    hasIconOnly
-                    iconDescription="Download"
-                    tooltipPosition="bottom"
-                    kind="ghost"
-                    onClick={() => null}
-                    renderIcon={Icons.Download}
-                    size="md"
-                  />
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-          ):null}
+              {filteredElements.map((item, idx) => (
+                <TableRow key={hashObject(item)} id={`row${idx}`}>
+                  {Object.keys(item).map((key) =>
+                    createTableCell(item, key, idx)
+                  )}
+                  <TableCell className="activities-table-action">
+                    <Button
+                      hasIconOnly
+                      iconDescription="View"
+                      tooltipPosition="bottom"
+                      kind="ghost"
+                      onClick={() => clickFn(item.id)}
+                      renderIcon={Icons.DataViewAlt}
+                      size="md"
+                    />
+                    <Button
+                      hasIconOnly
+                      iconDescription="Download"
+                      tooltipPosition="bottom"
+                      kind="ghost"
+                      onClick={() => null}
+                      renderIcon={Icons.Download}
+                      size="md"
+                    />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          ) : null}
         </Table>
       </TableContainer>
-      
-      {/* Check if therer are no elements in the table, if not then print the Empty */}
-      {elements.length<=0?(
+
+      {/* Check if there are no elements in the table, if not then print the Empty */}
+      {filteredElements.length <= 0 ? (
         <EmptySection
-        pictogram="Magnify"
-        title={'There are no openings to show yet'}
-        description={'Your recent openings will appear here once you generate one'}
-        fill = {'#0073E6'}
-      />
-      ):null}
+          pictogram="Magnify"
+          title={'There are no openings to show yet'}
+          description={
+            'Your recent openings will appear here once you generate one'
+          }
+          fill="#0073E6"
+        />
+      ) : null}
     </>
   );
 };
