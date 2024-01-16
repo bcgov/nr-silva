1c912c3b6138c9a3fb72cc95c6eeb47828fdaf98
Ricardo Campos <ricardo.campos@encora.com>
2024-01-16 17:35:12 -0300

    feat: enable backend logs to be sent to opensearch (#217)

---
+++ backend/src/main/java/ca/bc/gov/restapi/results/Controller.java
@@ -0,0 +1,29 @@
+package ca.bc.gov.restapi.results;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import java.time.LocalDateTime;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/** This class contains a simple controler with a single endpoint for testing purposes. */
+@Tag(name = "Controller", description = "Simple controller class for testing")
+@RestController
+@RequestMapping("/test")
+@Slf4j
+public class Controller {
+
+  /**
+   * Simply logs the current date and time.
+   *
+   * @return A string containing current date and time.
+   */
+  @GetMapping
+  public String testRequest() {
+    String template = "Hello, current date time is %s";
+    String finalMessage = String.format(template, LocalDateTime.now());
+    log.info(finalMessage);
+    return finalMessage;
+  }
+}
