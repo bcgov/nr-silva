7bfd5038f0445214f1ffdb29efa4fb089e704e67
Ricardo Campos <ricardo.campos@encora.com>
2024-01-24 11:53:47 -0300

    feat: increase memory limits and fix certificate path

--- backend/dockerfile-entrypoint.sh
+++ backend/dockerfile-entrypoint.sh
@@ -4,9 +4,6 @@ java -cp /app/artifacts/ InstallCert --quiet "${DATABASE_HOST}:${DATABASE_PORT}"
 keytool -exportcert -alias "${DATABASE_HOST}-1" -keystore jssecacerts -storepass changeit -file oracle.cer
 keytool -importcert -alias orakey -noprompt -cacerts -storepass changeit -file oracle.cer
 
-pwd
-ls -l artifacts/
-
 java \
     -Djava.security.egd=file:/dev/./urandom \
     ${JAVA_OPTS} \
