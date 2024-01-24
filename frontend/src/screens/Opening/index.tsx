74093c73fb80fb6050e628152f9ea9496094e027
Jazz Grewal <39718912+jazzgrewal@users.noreply.github.com>
2024-01-24 10:06:30 -0800

    Fix/opening design (#221)

--- frontend/src/screens/Opening/index.tsx
+++ frontend/src/screens/Opening/index.tsx
@@ -9,6 +9,9 @@ import { recentOpeningItems, recentOpeningItems1, recentOpeningsHeader } from ".
 import './Opening.scss'
 import { ViewFilled } from '@carbon/icons-react';
 import OpeningsMap from "../../components/OpeningsMap";
+import OpeningScreenDataTable from "../../components/OpeningScreenDataTable/index";
+import { headers, rows } from "../../components/OpeningScreenDataTable/testData";
+import SectionTitle from "../../components/SectionTitle";
 
 
 const Opening: React.FC = () => {
@@ -26,32 +29,32 @@ const Opening: React.FC = () => {
     return (
       <>
       <div className="container-fluid">
-        <div className="row p-4">
+        <div className="row px-0 py-4 p-md-4">
           <PageTitle title="Openings" subtitle="Create, manage or check opening information " />
         </div>
       </div>
 
       <div className="favourite-activities">
         <div className="row">
-          <div className="col-md-6 col-lg-4 col-xl-3">
-            <FavouriteCard index={1} title={'Search opening'} link={'/'} icon={'Dashboard'} description={'Search for a opening to update or consult using advance filters such as activities, standards, codes or links'}/>
+          <div className="col-md-6 col-lg-6 col-xl-6 col-xxl-3">
+            <FavouriteCard index={1} title={'Search opening'} link={'/'} icon={'SearchLocate'} description={'Search for a opening to update or consult using advance filters such as activities, standards, codes or links'}/>
           </div>
-          <div className="col-md-6 col-lg-4 col-xl-3">
+          <div className="col-md-6 col-lg-6 col-xl-6 col-xxl-3">
             <FavouriteCard index={1} title={'Create a new opening'} link={'/'} icon={'MapBoundary'} description={'Register a opening for tracking silviculture treatments'}/>
           </div>
-          <div className="col-md-6 col-lg-4 col-xl-3">
+          <div className="col-md-6 col-lg-6 col-xl-6 col-xxl-3">
             <FavouriteCard index={1} title={'Reports'} link={'/'} icon={'ReportData'} description={'Consult and manage opening reports'}/>
           </div>
-          <div className="col-md-6 col-lg-4 col-xl-3">
+          <div className="col-md-6 col-lg-6 col-xl-6 col-xxl-3">
             <FavouriteCard index={1} title={'Upcoming activities'} link={'/'} icon={'Activity'} description={'Track opening activities and milestones'}/>
           </div>
         </div>
       </div>
 
       <div className="container-fluid">
-        <div className="row p-4">
-            <PageTitle title="Recent Openings" subtitle="Track your recent openings and select to check spatial activity" />
-            <Button className="h-100 my-auto" renderIcon={ViewFilled} type="button" onClick={toggleSpatial}>
+        <div className="row px-0 py-4 p-md-4">
+            <SectionTitle title="Recent openings" subtitle="Track your recent openings and select to check spatial activity" />
+            <Button className="h-100 my-auto d-none d-md-block" renderIcon={ViewFilled} type="button" onClick={toggleSpatial}>
               {showSpatial?'Hide Spatial':'Show Spatial'}
             </Button>
         </div>
@@ -70,11 +73,10 @@ const Opening: React.FC = () => {
       </div>
       
 
-      <div className="container-fluid">
-        <RecentOpeningsTable
-          headers={recentOpeningsHeader}
-          elements={recentOpeningItems}
-          clickFn={goToActivity}
+      <div className="container-fluid p-0">
+        <OpeningScreenDataTable
+        headers={headers}
+        rows={rows}
         />
       </div>
       </>
