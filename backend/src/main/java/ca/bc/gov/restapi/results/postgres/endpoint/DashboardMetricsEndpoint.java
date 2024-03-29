9e00586b6f72d4cadea99f4f436f8a38cfc85cbc
Ricardo Campos <ricardo.campos@encora.com>
2024-03-29 18:29:54 -0300

    docs: add swagger open-api docs to the endpoint

--- backend/src/main/java/ca/bc/gov/restapi/results/postgres/endpoint/DashboardMetricsEndpoint.java
+++ backend/src/main/java/ca/bc/gov/restapi/results/postgres/endpoint/DashboardMetricsEndpoint.java
@@ -3,6 +3,12 @@ package ca.bc.gov.restapi.results.postgres.endpoint;
 import ca.bc.gov.restapi.results.postgres.dto.OpeningsPerYearDto;
 import ca.bc.gov.restapi.results.postgres.dto.OpeningsPerYearFiltersDto;
 import ca.bc.gov.restapi.results.postgres.service.DashboardMetricsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -19,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 /** This class holds resources for the dashboard metrics page. */
 @RestController
 @RequestMapping("/api/dashboard-metrics")
-@Tag(name = "Dashboard Metrics")
+@Tag(name = "Dashboard Metrics", description = "Resources fot the Dashboard metrics charts")
 @RequiredArgsConstructor
 public class DashboardMetricsEndpoint {
 
@@ -35,11 +41,54 @@ public class DashboardMetricsEndpoint {
    * @return A list of values to populate the chart or 204 no content if no data.
    */
   @GetMapping("/submission-trends")
+  @Operation(
+      summary = "Get data for the Submission Trends Chart, Openings per Year",
+      description = "Fetches data from the last years for the openings per year chart.",
+      responses = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "An array with twelve objects for the last 12 months."),
+        @ApiResponse(
+            responseCode = "204",
+            description = "No data found in the dable. No response body."),
+        @ApiResponse(
+            responseCode = "401",
+            description = "Access token is missing or invalid",
+            content = @Content(schema = @Schema(implementation = Void.class)))
+      })
   public ResponseEntity<List<OpeningsPerYearDto>> getOpeningsSubmissionTrends(
-      @RequestParam(value = "orgUnit", required = false) String orgUnitCode,
-      @RequestParam(value = "status", required = false) String statusCode,
-      @RequestParam(value = "entryDateStart", required = false) String entryDateStart,
-      @RequestParam(value = "entryDateEnd", required = false) String entryDateEnd) {
+      @RequestParam(value = "orgUnitCode", required = false)
+          @Parameter(
+              name = "orgUnitCode",
+              in = ParameterIn.QUERY,
+              description = "The Org Unit code to filter, same as District",
+              required = false,
+              example = "DCR")
+          String orgUnitCode,
+      @RequestParam(value = "statusCode", required = false)
+          @Parameter(
+              name = "statusCode",
+              in = ParameterIn.QUERY,
+              description = "The Openins Status code to filter",
+              required = false,
+              example = "APP")
+          String statusCode,
+      @RequestParam(value = "entryDateStart", required = false)
+          @Parameter(
+              name = "entryDateStart",
+              in = ParameterIn.QUERY,
+              description = "The Openins entry timestamp start date to filter, format yyyy-MM-dd",
+              required = false,
+              example = "2024-03-11")
+          String entryDateStart,
+      @RequestParam(value = "entryDateEnd", required = false)
+          @Parameter(
+              name = "entryDateEnd",
+              in = ParameterIn.QUERY,
+              description = "The Openins entry timestamp end date to filter, format yyyy-MM-dd",
+              required = false,
+              example = "2024-03-11")
+          String entryDateEnd) {
     LocalDateTime entryDateStartDate = null;
     LocalDateTime entryDateEndDate = null;
     DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd");
