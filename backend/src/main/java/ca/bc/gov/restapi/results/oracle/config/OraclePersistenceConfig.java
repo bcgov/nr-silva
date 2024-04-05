36601b5b131b0b44802ee04fb94ce94bbcb7ea3e
Ricardo Campos <ricardo.campos@encora.com>
2024-04-05 16:42:29 -0300

    fix: add graceful shutdown and hikari connection pool (#274)

--- backend/src/main/java/ca/bc/gov/restapi/results/oracle/config/OraclePersistenceConfig.java
+++ backend/src/main/java/ca/bc/gov/restapi/results/oracle/config/OraclePersistenceConfig.java
@@ -1,6 +1,9 @@
 package ca.bc.gov.restapi.results.oracle.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -11,15 +14,32 @@ import org.springframework.context.annotation.Primary;
 @Configuration
 public class OraclePersistenceConfig {
 
+  @Autowired private OracleHikariConfig oracleHikariConfig;
+
   @Bean
   @ConfigurationProperties("spring.datasource.oracle")
   public DataSourceProperties oracleDataSourceProperties() {
     return new DataSourceProperties();
   }
 
+  /** Creates a Postgres Datasource with all Hikari connection pool configuration. */
   @Bean
   @Primary
   public DataSource oracleDataSource() {
-    return oracleDataSourceProperties().initializeDataSourceBuilder().build();
+    HikariConfig config = new HikariConfig();
+    config.setJdbcUrl(oracleHikariConfig.getUrl());
+    config.setUsername(oracleHikariConfig.getUsername());
+    config.setPassword(oracleHikariConfig.getPassword());
+    config.setDriverClassName(oracleHikariConfig.getDriverClassName());
+    config.setConnectionTimeout(oracleHikariConfig.getConnectionTimeout());
+    config.setIdleTimeout(oracleHikariConfig.getIdleTimeout());
+    config.setMaxLifetime(oracleHikariConfig.getMaxLifetime());
+    config.setKeepaliveTime(oracleHikariConfig.getKeepaliveTime());
+    config.setPoolName(oracleHikariConfig.getPoolName());
+    config.setMinimumIdle(oracleHikariConfig.getMinimumIdle());
+    config.setMaximumPoolSize(oracleHikariConfig.getMaximumPoolSize());
+    config.setLeakDetectionThreshold(oracleHikariConfig.getLeakDetectionThreshold());
+
+    return new HikariDataSource(config);
   }
 }
