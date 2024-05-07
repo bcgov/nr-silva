817c3eeeb7878460738122f6f85667728fe0f3b1
Ricardo Campos <ricardo.campos@encora.com>
2024-05-07 17:25:56 -0300

    chore: fix checkstyle

--- backend/src/main/java/ca/bc/gov/restapi/results/common/config/CorsConfig.java
+++ backend/src/main/java/ca/bc/gov/restapi/results/common/config/CorsConfig.java
@@ -1,13 +1,12 @@
 package ca.bc.gov.restapi.results.common.config;
 
 import java.util.Arrays;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import lombok.extern.slf4j.Slf4j;
 
 /** This class holds the configuration for CORS handling. */
 @Slf4j
