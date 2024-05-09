7119bf0fbff3e9fca358b370ec17fa8027cc23f5
Ricardo Campos <ricardo.campos@encora.com>
2024-05-09 15:48:27 -0300

    fix: warnings and errors on react components

--- frontend/src/components/BarChartGrouped/index.jsx
+++ frontend/src/components/BarChartGrouped/index.jsx
@@ -132,10 +132,22 @@ const BarChartGrouped = () => {
     <div className="px-3">
       <div className="row gy-2 pb-3">
         <div className="col-md-4 p-0">
-          <Dropdown id="default" label={windowWidth<=1584?"District":"Filter by district"} items={items} itemToString={item => item ? item.text : ''} />
+          <Dropdown
+            id="default1"
+            label={windowWidth<=1584?"District":"Filter by district"}
+            items={items}
+            itemToString={item => item ? item.text : ''}
+            titleText={windowWidth<=1584?"District":"Filter by district"}
+          />
         </div>
         <div className="col-md-4 p-0 px-md-1">
-          <Dropdown id="default" label={windowWidth<=1584?"Status":"Filter by status"} items={items} itemToString={item => item ? item.text : ''} />
+          <Dropdown
+            id="default2"
+            label={windowWidth<=1584?"Status":"Filter by status"}
+            items={items}
+            itemToString={item => item ? item.text : ''}
+            titleText={windowWidth<=1584?"Status":"Filter by status"}
+          />
         </div>
         <div className="col-4 p-0 d-none d-md-block">
           <DatePicker datePickerType="single">
@@ -143,6 +155,7 @@ const BarChartGrouped = () => {
               id="date-picker-input-id"
               placeholder="mm/dd/yyyy"
               size="md"
+              labelText="Label text"
             />
           </DatePicker>
         </div>
