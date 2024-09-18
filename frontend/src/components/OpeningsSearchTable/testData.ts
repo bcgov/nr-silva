ef9177d202e6b1c5d24a5b08796b5d480cf80a27
jazzgrewal <jaski.grewal@gmail.com>
2024-09-18 14:45:33 -0700

    corrected the selected key issue in previous build

--- frontend/src/components/OpeningsSearchTable/testData.ts
+++ frontend/src/components/OpeningsSearchTable/testData.ts
@@ -3,42 +3,53 @@ import { ITableHeader } from "../../types/TableHeader";
 export const headers: ITableHeader[] = [
   {
     key: 'openingId',
-    header: 'Opening Id'
+    header: 'Opening Id',
+    selected: true,
   },
   {
     key: 'forestFileId',
-    header: 'File Id'
+    header: 'File Id',
+    selected: true,
   },
   {
     key: 'cuttingPermit',
-    header: 'Cutting permit'
+    header: 'Cutting permit',
+    selected: true,
   },
   {
     key: 'timberMark',
-    header: 'Timber mark'
+    header: 'Timber mark',
+    selected: true,
   },
   {
     key: 'cutBlock',
-    header: 'Cut block'
+    header: 'Cut block',
+    selected: true,
   },
   {
     key: 'grossAreaHa',
-    header: 'Gross area (ha)'
+    header: 'Gross area (ha)',
+    selected: true,
   },
   {
     key: 'status',
-    header: 'Status'
+    header: 'Status',
+    selected: false,
   },
   {
     key: 'category',
-    header: 'Category'
+    header: 'Category',
+    selected: false,
   },
   {
     key: 'disturbanceStart',
-    header: 'Disturbance start'
+    header: 'Disturbance start',
+    selected: true,
   },
   {
     key: 'actions',
-    header: 'Actions'
+    header: 'Actions',
+    selected: true,
   }
 ];
+
