e588b356abc3db0b5c09db945af5a5f49f418da9
jazzgrewal <jaski.grewal@gmail.com>
2024-09-25 11:40:50 -0700

    added the build failure fix

--- frontend/src/components/OpeningScreenDataTable/testData.ts
+++ frontend/src/components/OpeningScreenDataTable/testData.ts
@@ -3,42 +3,53 @@ import { ITableHeader } from "../../types/TableHeader";
 export const headers: ITableHeader[] = [
   {
     key: 'openingId',
-    header: 'Opening Id'
+    header: 'Opening Id',
+    selected: false
   },
   {
     key: 'forestFileId',
-    header: 'File Id'
+    header: 'File Id',
+    selected: false
   },
   {
     key: 'cuttingPermit',
-    header: 'Cutting permit'
+    header: 'Cutting permit',
+    selected: false
   },
   {
     key: 'timberMark',
-    header: 'Timber mark'
+    header: 'Timber mark',
+    selected: false
   },
   {
     key: 'cutBlock',
-    header: 'Cut block'
+    header: 'Cut block',
+    selected: false
   },
   {
     key: 'grossAreaHa',
-    header: 'Gross area (ha)'
+    header: 'Gross area (ha)',
+    selected: false
   },
   {
     key: 'status',
-    header: 'Status'
+    header: 'Status',
+    selected: false
   },
   {
     key: 'category',
-    header: 'Category'
+    header: 'Category',
+    selected: false
   },
   {
     key: 'disturbanceStart',
-    header: 'Disturbance start'
+    header: 'Disturbance start',
+    selected: false
   },
   {
     key: 'actions',
-    header: 'Actions'
+    header: 'Actions',
+    selected: false
   }
 ];
+
