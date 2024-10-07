233c962cd09491f93f13dc2afddfcfd52bfbb2b4
Ricardo Campos <ricardo.campos@encora.com>
2024-10-07 12:06:57 -0300

    fix: build issues due to typing problems

--- frontend/src/amplifyconfiguration.ts
+++ frontend/src/amplifyconfiguration.ts
@@ -10,20 +10,23 @@ const redirectSignOut =
     ? env.VITE_REDIRECT_SIGN_OUT
     : `${logoutDomain}/clp-cgi/logoff.cgi?retnow=1&returl=${retUrl}?redirect_uri=${redirectUri}/`;
 
+type verificationMethodsType = 'code' | 'token';
+const verificationMethods: verificationMethodsType = 'code';
+
 // https://docs.amplify.aws/javascript/build-a-backend/auth/set-up-auth/
 const amplifyconfig = {
   Auth: {
     Cognito: {
       userPoolId: env.VITE_USER_POOLS_ID ?? "ca-central-1_t2HSZBHur",
       userPoolClientId: env.VITE_USER_POOLS_WEB_CLIENT_ID ?? "70a2am185rie10r78b0ugcs1mm",
-      signUpVerificationMethod: 'code', // 'code' | 'link'
+      signUpVerificationMethod: verificationMethods, // 'code' | 'link'
       loginWith: {
         oauth: {
           domain: env.VITE_AWS_DOMAIN || "prod-fam-user-pool-domain.auth.ca-central-1.amazoncognito.com",
           scopes: ['openid'],
           redirectSignIn: [`${window.location.origin}/dashboard`],
           redirectSignOut: [redirectSignOut],
-          responseType: 'code'
+          responseType: verificationMethods
         }
       }
     }
