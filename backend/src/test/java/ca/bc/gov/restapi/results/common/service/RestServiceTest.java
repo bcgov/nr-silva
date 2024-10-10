76ee424128bceb875d15c2ae58705a8afe03e37d
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2024-10-10 14:41:31 -0700

    feat: adding wiremock and updating tests

--- backend/src/test/java/ca/bc/gov/restapi/results/common/service/RestServiceTest.java
+++ backend/src/test/java/ca/bc/gov/restapi/results/common/service/RestServiceTest.java
@@ -1,48 +1,69 @@
 package ca.bc.gov.restapi.results.common.service;
 
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.notFound;
+import static com.github.tomakehurst.wiremock.client.WireMock.okJson;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 
+import ca.bc.gov.restapi.results.extensions.WiremockLogNotifier;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.RestClient;
 
-@RestClientTest(RestService.class)
 class RestServiceTest {
 
-  @Autowired private RestService restService;
+  @RegisterExtension
+  static WireMockExtension clientApiStub = WireMockExtension
+      .newInstance()
+      .options(
+          wireMockConfig()
+              .port(10001)
+              .notifier(new WiremockLogNotifier())
+              .asynchronousResponseEnabled(true)
+              .stubRequestLoggingDisabled(false)
+      )
+      .configureStaticDsl(true)
+      .build();
 
-  @Autowired private MockRestServiceServer mockRestServiceServer;
+  private final RestService restService = new RestService(
+      RestClient.builder().baseUrl("http://localhost:10001").build()
+  );
 
   @Test
   @DisplayName("Get opening polygon and properties happy path should succeed")
   void getOpeningPolygonAndProperties_happyPath_shouldSucceed() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("https://openmaps.gov.bc.ca/geo/ows");
-    sb.append("?service=WFS");
-    sb.append("&version=2.0.0");
-    sb.append("&request=GetFeature");
-    sb.append("&typeName=WHSE_FOREST_VEGETATION.RSLT_OPENING_SVW");
-    sb.append("&outputFormat=application/json");
-    sb.append("&SrsName=EPSG:4326");
-    sb.append("&PROPERTYNAME=");
-    sb.append("OPENING_ID,GEOMETRY,REGION_NAME,REGION_CODE,DISTRICT_NAME,DISTRICT_CODE,");
-    sb.append("CLIENT_NAME,CLIENT_NUMBER,OPENING_WHEN_CREATED");
 
     String openingId = "123";
-    sb.append("&CQL_FILTER=OPENING_ID%3D").append(openingId);
-
-    String json = "{}";
-
-    mockRestServiceServer
-        .expect(requestTo(sb.toString()))
-        .andRespond(withSuccess(json, MediaType.APPLICATION_JSON));
+    clientApiStub
+        .stubFor(
+            get(urlPathEqualTo("/"))
+                .withQueryParam("service", equalTo("WFS"))
+                .withQueryParam("version", equalTo("2.0.0"))
+                .withQueryParam("request", equalTo("GetFeature"))
+                .withQueryParam("typeName", equalTo("WHSE_FOREST_VEGETATION.RSLT_OPENING_SVW"))
+                .withQueryParam("outputFormat", equalTo("application/json"))
+                .withQueryParam("SrsName", equalTo("EPSG:4326"))
+                .withQueryParam("PROPERTYNAME",
+                    equalTo("OPENING_ID," +
+                        "GEOMETRY," +
+                        "REGION_NAME," +
+                        "REGION_CODE," +
+                        "DISTRICT_NAME," +
+                        "DISTRICT_CODE," +
+                        "CLIENT_NAME," +
+                        "CLIENT_NUMBER," +
+                        "OPENING_WHEN_CREATED"
+                    ))
+                .withQueryParam("CQL_FILTER", equalTo("OPENING_ID=" + openingId))
+                .willReturn(okJson("{}"))
+        );
 
     Object response = restService.getOpeningPolygonAndProperties(openingId);
 
@@ -52,24 +73,31 @@ class RestServiceTest {
   @Test
   @DisplayName("Get opening polygon and properties not found should fail")
   void getOpeningPolygonAndProperties_notFound_shouldFail() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("https://openmaps.gov.bc.ca/geo/ows");
-    sb.append("?service=WFS");
-    sb.append("&version=2.0.0");
-    sb.append("&request=GetFeature");
-    sb.append("&typeName=WHSE_FOREST_VEGETATION.RSLT_OPENING_SVW");
-    sb.append("&outputFormat=application/json");
-    sb.append("&SrsName=EPSG:4326");
-    sb.append("&PROPERTYNAME=");
-    sb.append("OPENING_ID,GEOMETRY,REGION_NAME,REGION_CODE,DISTRICT_NAME,DISTRICT_CODE,");
-    sb.append("CLIENT_NAME,CLIENT_NUMBER,OPENING_WHEN_CREATED");
 
     String openingId = "123";
-    sb.append("&CQL_FILTER=OPENING_ID%3D").append(openingId);
-
-    mockRestServiceServer
-        .expect(requestTo(sb.toString()))
-        .andRespond(withStatus(HttpStatusCode.valueOf(404)));
+    clientApiStub
+        .stubFor(
+            get(urlPathEqualTo("/"))
+                .withQueryParam("service", equalTo("WFS"))
+                .withQueryParam("version", equalTo("2.0.0"))
+                .withQueryParam("request", equalTo("GetFeature"))
+                .withQueryParam("typeName", equalTo("WHSE_FOREST_VEGETATION.RSLT_OPENING_SVW"))
+                .withQueryParam("outputFormat", equalTo("application/json"))
+                .withQueryParam("SrsName", equalTo("EPSG:4326"))
+                .withQueryParam("PROPERTYNAME",
+                    equalTo("OPENING_ID," +
+                        "GEOMETRY," +
+                        "REGION_NAME," +
+                        "REGION_CODE," +
+                        "DISTRICT_NAME," +
+                        "DISTRICT_CODE," +
+                        "CLIENT_NAME," +
+                        "CLIENT_NUMBER," +
+                        "OPENING_WHEN_CREATED"
+                    ))
+                .withQueryParam("CQL_FILTER", equalTo("OPENING_ID=" + openingId))
+                .willReturn(notFound())
+        );
 
     Object response = restService.getOpeningPolygonAndProperties(openingId);
 
