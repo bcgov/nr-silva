5ceb7e6f1c3d94467aba570889b79023e030321a
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2024-10-28 05:40:02 -0700

    chore(deps): removing swagger
    Swagger is not required for the backend api as the only consumer should be the frontend app

--- backend/src/main/java/ca/bc/gov/restapi/results/postgres/dto/MyRecentActionsRequestsDto.java
+++ backend/src/main/java/ca/bc/gov/restapi/results/postgres/dto/MyRecentActionsRequestsDto.java
@@ -1,6 +1,5 @@
 package ca.bc.gov.restapi.results.postgres.dto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.With;
@@ -11,30 +10,12 @@ import lombok.With;
 @Builder
 @With
 public record MyRecentActionsRequestsDto(
-    @Schema(description = "Full description of the action made by the user.", example = "Update")
     String activityType,
-    @Schema(
-        description = "System generated value uniquely identifying the opening.",
-        example = "1541297")
     Long openingId,
-    @Schema(
-        description = """
-            A code indicating the status of the prescription. Examples include but are not
-            limited to DFT (draft) and APP (approved). A subset of the STATUS_CODE table.
-            """,
-        example = "APP")
     String statusCode,
-    @Schema(
-        description = """
-            The code description indicating the status of the prescription. Examples include but
-            are not limited to Draft (DFT) and Approved (APP). A subset of the STATUS_CODE table.
-            """,
-        example = "Approved")
     String statusDescription,
-    @Schema(
-        description = "The date and time of the activity action in for 'time ago' format",
-        example = "1 minute ago")
     String lastUpdatedLabel,
-    @Schema(description = "The date and time of the activity action") LocalDateTime lastUpdated) {
+    LocalDateTime lastUpdated
+) {
 
 }
--- backend/src/main/java/ca/bc/gov/restapi/results/postgres/endpoint/DashboardMetricsEndpoint.java
+++ backend/src/main/java/ca/bc/gov/restapi/results/postgres/endpoint/DashboardMetricsEndpoint.java
@@ -6,13 +6,6 @@ import ca.bc.gov.restapi.results.postgres.dto.FreeGrowingMilestonesDto;
 import ca.bc.gov.restapi.results.postgres.dto.MyRecentActionsRequestsDto;
 import ca.bc.gov.restapi.results.postgres.dto.OpeningsPerYearDto;
 import ca.bc.gov.restapi.results.postgres.service.DashboardMetricsService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -21,12 +14,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-/** This class holds resources for the dashboard metrics page. */
+/**
+ * This class holds resources for the dashboard metrics page.
+ */
 @RestController
 @RequestMapping("/api/dashboard-metrics")
-@Tag(
-    name = "Dashboard Metrics (Postgres)",
-    description = "Endpoints for the Dashboard metrics charts on `SILVA` schema")
 @RequiredArgsConstructor
 public class DashboardMetricsEndpoint {
 
@@ -36,62 +28,22 @@ public class DashboardMetricsEndpoint {
    * Gets data for the Opening submission trends Chart (Openings per year) on the Dashboard SILVA
    * page.
    *
-   * @param orgUnitCode Optional district code filter.
-   * @param statusCode Optional opening status code filter.
+   * @param orgUnitCode    Optional district code filter.
+   * @param statusCode     Optional opening status code filter.
    * @param entryDateStart Optional opening entry timestamp start date filter.
-   * @param entryDateEnd Optional opening entry timestamp end date filter.
+   * @param entryDateEnd   Optional opening entry timestamp end date filter.
    * @return A list of values to populate the chart or 204 no content if no data.
    */
   @GetMapping("/submission-trends")
-  @Operation(
-      summary = "Gets data for the Opening submission trends Chart (Openings per year).",
-      description = "Fetches data from the last twelve months for the openings per year chart.",
-      responses = {
-        @ApiResponse(
-            responseCode = "200",
-            description = "An array with twelve objects for the last 12 months.",
-            content = @Content(mediaType = "application/json")),
-        @ApiResponse(
-            responseCode = "204",
-            description = "No data found on the table. No response body."),
-        @ApiResponse(
-            responseCode = "401",
-            description = "Access token is missing or invalid",
-            content = @Content(schema = @Schema(implementation = Void.class)))
-      })
   public ResponseEntity<List<OpeningsPerYearDto>> getOpeningsSubmissionTrends(
       @RequestParam(value = "orgUnitCode", required = false)
-          @Parameter(
-              name = "orgUnitCode",
-              in = ParameterIn.QUERY,
-              description = "The Org Unit code to filter, same as District",
-              required = false,
-              example = "DCR")
-          String orgUnitCode,
+      String orgUnitCode,
       @RequestParam(value = "statusCode", required = false)
-          @Parameter(
-              name = "statusCode",
-              in = ParameterIn.QUERY,
-              description = "The Openings Status code to filter",
-              required = false,
-              example = "APP")
-          String statusCode,
+      String statusCode,
       @RequestParam(value = "entryDateStart", required = false)
-          @Parameter(
-              name = "entryDateStart",
-              in = ParameterIn.QUERY,
-              description = "The Openings entry timestamp start date to filter, format yyyy-MM-dd",
-              required = false,
-              example = "2024-03-11")
-          String entryDateStart,
+      String entryDateStart,
       @RequestParam(value = "entryDateEnd", required = false)
-          @Parameter(
-              name = "entryDateEnd",
-              in = ParameterIn.QUERY,
-              description = "The Openings entry timestamp end date to filter, format yyyy-MM-dd",
-              required = false,
-              example = "2024-03-11")
-          String entryDateEnd) {
+      String entryDateEnd) {
     DashboardFiltersDto filtersDto =
         new DashboardFiltersDto(
             orgUnitCode,
@@ -113,64 +65,22 @@ public class DashboardMetricsEndpoint {
   /**
    * Gets data for the Free growing Chart on the Dashboard SILVA page.
    *
-   * @param orgUnitCode Optional district code filter.
-   * @param clientNumber Optional client number filter.
+   * @param orgUnitCode    Optional district code filter.
+   * @param clientNumber   Optional client number filter.
    * @param entryDateStart Optional opening entry timestamp start date filter.
-   * @param entryDateEnd Optional opening entry timestamp end date filter.
+   * @param entryDateEnd   Optional opening entry timestamp end date filter.
    * @return A list of values to populate the chart or 204 no content if no data.
    */
   @GetMapping("/free-growing-milestones")
-  @Operation(
-      summary = "Gets data for the Free growing Chart on the Dashboard SILVA page.",
-      description =
-          "Fetches data from the last 24 months and group them into periods for the Free growing"
-              + " chart.",
-      responses = {
-        @ApiResponse(
-            responseCode = "200",
-            description = "An array with four objects, one for each piece of the chart.",
-            content = @Content(mediaType = "application/json")),
-        @ApiResponse(
-            responseCode = "204",
-            description = "No data found on the table. No response body."),
-        @ApiResponse(
-            responseCode = "401",
-            description = "Access token is missing or invalid",
-            content = @Content(schema = @Schema(implementation = Void.class)))
-      })
   public ResponseEntity<List<FreeGrowingMilestonesDto>> getFreeGrowingMilestonesData(
       @RequestParam(value = "orgUnitCode", required = false)
-          @Parameter(
-              name = "orgUnitCode",
-              in = ParameterIn.QUERY,
-              description = "The Org Unit code to filter, same as District",
-              required = false,
-              example = "DCR")
-          String orgUnitCode,
+      String orgUnitCode,
       @RequestParam(value = "clientNumber", required = false)
-          @Parameter(
-              name = "clientNumber",
-              in = ParameterIn.QUERY,
-              description = "The Client Number to filter",
-              required = false,
-              example = "00012797")
-          String clientNumber,
+      String clientNumber,
       @RequestParam(value = "entryDateStart", required = false)
-          @Parameter(
-              name = "entryDateStart",
-              in = ParameterIn.QUERY,
-              description = "The Openings entry timestamp start date to filter, format yyyy-MM-dd",
-              required = false,
-              example = "2024-03-11")
-          String entryDateStart,
+      String entryDateStart,
       @RequestParam(value = "entryDateEnd", required = false)
-          @Parameter(
-              name = "entryDateEnd",
-              in = ParameterIn.QUERY,
-              description = "The Openings entry timestamp end date to filter, format yyyy-MM-dd",
-              required = false,
-              example = "2024-03-11")
-          String entryDateEnd) {
+      String entryDateEnd) {
     DashboardFiltersDto filtersDto =
         new DashboardFiltersDto(
             orgUnitCode,
@@ -194,22 +104,6 @@ public class DashboardMetricsEndpoint {
    * @return A list of values to populate the chart or 204 no content if no data.
    */
   @GetMapping("/my-recent-actions/requests")
-  @Operation(
-      summary = "Gets the last 5 most recent updated openings for the request user.",
-      description = "Fetches data for the My recent actions table, Requests tab",
-      responses = {
-        @ApiResponse(
-            responseCode = "200",
-            description = "An array with five objects, one for opening row.",
-            content = @Content(mediaType = "application/json")),
-        @ApiResponse(
-            responseCode = "204",
-            description = "No data found for the user. No response body."),
-        @ApiResponse(
-            responseCode = "401",
-            description = "Access token is missing or invalid",
-            content = @Content(schema = @Schema(implementation = Void.class)))
-      })
   public ResponseEntity<List<MyRecentActionsRequestsDto>> getUserRecentOpeningsActions() {
     List<MyRecentActionsRequestsDto> actionsDto =
         dashboardMetricsService.getUserRecentOpeningsActions();
