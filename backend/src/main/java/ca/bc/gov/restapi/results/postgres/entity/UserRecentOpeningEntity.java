2a7e76d8936129a4357956df4cb04acff09378ee
jazzgrewal <jaski.grewal@gmail.com>
2024-10-29 15:33:35 -0700

    properly configured wiremock

--- backend/src/main/java/ca/bc/gov/restapi/results/postgres/entity/UserRecentOpeningEntity.java
+++ backend/src/main/java/ca/bc/gov/restapi/results/postgres/entity/UserRecentOpeningEntity.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "user_recent_openings")
+@Table(schema = "silva", name = "user_recent_openings")
 public class UserRecentOpeningEntity {
 
     @Id
