9a7fda094e593f4798d4ea6b168fa60396881799
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2024-11-07 05:23:40 -0800

    chore: fixing sonar issues

--- frontend/src/components/ThemeToggle/index.tsx
+++ frontend/src/components/ThemeToggle/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './ThemeToggle.scss';
 import { AsleepFilled, LightFilled } from '@carbon/icons-react';
 import { useThemePreference } from '../../utils/ThemePreference';
@@ -6,7 +6,7 @@ import { toggleTheme } from '../../utils/ThemeFunction';
 
 const ThemeToggle = () => {
   const { theme, setTheme } = useThemePreference();
-  const [isToggled, setIsToggled] = useState(theme === 'g10'?false:true);
+  const [isToggled, setIsToggled] = useState(theme !== 'g10');
 
   useEffect(()=>{
     setIsToggled(theme === 'g10'?false:true);
@@ -18,7 +18,17 @@ const ThemeToggle = () => {
   };
 
   return (
-    <div className={`theme-toggle ${isToggled ? 'on' : 'off'}`} onClick={handleToggle}>
+    <div
+      className={`theme-toggle ${isToggled ? 'on' : 'off'}`}
+      onClick={handleToggle}
+      role="button"
+      tabIndex={0}
+      onKeyDown={(e) => {
+        if (e.key === 'Enter' || e.key === ' ') {
+          handleToggle();
+        }
+      }}
+    >
       <div className="circle">
         {isToggled ? <AsleepFilled className="icon" /> : <LightFilled className="icon" />}
       </div>
