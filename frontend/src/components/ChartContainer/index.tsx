6227e23ef31869d0d8dbe03b675f3ffce9a885d8
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2024-11-08 10:24:42 -0800

    chore: removed the maximize and download options

--- frontend/src/components/ChartContainer/index.tsx
+++ frontend/src/components/ChartContainer/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import './ChartContainer.scss'
 import ChartTitle from '../ChartTitle';
-import { Button } from '@carbon/react';
-import * as Icons from '@carbon/icons-react';
 
 type Props = {
   children?: React.ReactNode;
@@ -24,26 +22,6 @@ function ChartContainer({ children, title, description }: Props): JSX.Element {
     <div className='chart-container'>
         <div className="d-flex flex-row">
           <ChartTitle title={title} subtitle={description} />
-          <div className='my-auto px-2'>
-            <Button
-              hasIconOnly
-              iconDescription="Maximize"
-              tooltipposition="bottom"
-              kind="ghost"
-              onClick={() => console.log('Download Click')}
-              renderIcon={Icons.Maximize}
-              size="md"
-            />
-            <Button
-              hasIconOnly
-              iconDescription="Download options"
-              tooltipposition="bottom"
-              kind="ghost"
-              onClick={() => console.log('Download Click')}
-              renderIcon={Icons.OverflowMenuVertical}
-              size="md"
-            />
-          </div>
         </div>
         {children? (
           <div className="content">
