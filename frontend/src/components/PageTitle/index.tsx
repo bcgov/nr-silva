fa6f008b6845027d50c262b1d3b7c50ed48f3f72
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2024-11-13 15:43:20 -0800

    chore: reducing complexity

--- frontend/src/components/PageTitle/index.tsx
+++ frontend/src/components/PageTitle/index.tsx
@@ -20,35 +20,19 @@ const PageTitle: React.FC<PageTitleProps> = ({
 
   // This will return up to the second level, even if we use just the first one
   const extractCurrentItems = (): LeftMenuItem[] => {
-    // Get the current location to use as lookup
     const currentLocation = useLocation().pathname;
-    // Recursive function to find the full path to the current item, retaining full objects
-    for (const item of leftMenu) {    
-      // Check for match at subItem level
+
+    for (const item of leftMenu) {
       if (item.items) {
         for (const subItem of item.items) {
-          //Create as an array for the sake of the return type
-          const subPath = [subItem];
-
-          // Check for match at subItem level, ideally it should never match as this is a high level item
-          // but it can happen
           if (subItem.link === currentLocation) {
-            return subPath; // Direct match, return the path
+            return [subItem];
           }
 
-          // Check for match at subSubItem level, and this is usually the case for most of the
-          // items. This is the most common scenario, and for that, we will return both the item
-          // and the parent
           if (subItem.subItems) {
             for (const subSubItem of subItem.subItems) {
-              // Create an array, so we can generate the breadcrumb with the deeper levels              
-              if (subSubItem.link === currentLocation) {                
-                // We're returning the subPath here because we want to stop at the first level
-                // This is because so far we have just two levels, and we don't want to go deeper in the breadcrumb
-                // In case we wanted, we could just do this before this if and return the fullPath instead
-                //const fullPath = [...subPath, subSubItem];
-                return subPath; 
-
+              if (subSubItem.link === currentLocation) {
+                return [subItem];
               }
             }
           }
