6dd766e26fa2eaef2720826fe940d90efc886942
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2024-11-19 14:21:31 -0800

    chore: updating commingsoon component

--- frontend/src/components/ComingSoonModal/index.tsx
+++ frontend/src/components/ComingSoonModal/index.tsx
@@ -1,3 +1,4 @@
+import React, {useEffect, useState} from "react";
 import { Modal } from "@carbon/react";
 interface IComingSoonModal {
   openingDetails: string;
@@ -6,19 +7,36 @@ interface IComingSoonModal {
 
 const ComingSoonModal: React.FC<IComingSoonModal> = ({
   openingDetails,
-  setOpeningDetails,
+  setOpeningDetails
 }) => {
+
+  const [open, setOpen] = useState<boolean>(false);
+  const [details, setDetails] = useState<string>(openingDetails || "");
+
+  useEffect(() => {
+    setDetails(openingDetails);
+    if(openingDetails) {
+      setOpen(true);
+    }
+  }, [openingDetails]);
+
+  const close = () => {
+    setOpen(false);
+    setOpeningDetails("");
+    setDetails("");
+  }
+
   return (
     <Modal
-      open={openingDetails.length > 0}
-      onRequestClose={() => setOpeningDetails("")}
+      open={open}
+      onRequestClose={() => close()}
       passiveModal
       modalHeading="Coming Soon"
-      modalLabel={`Opening ID: ${openingDetails}`}
+      modalLabel={`Opening ID: ${details}`}
     >
       <p className="modal-subtext">An opening details page is in development.</p> <br/>
       <p className="modal-subtext">
-        {` Until it's available, search for ${openingDetails} in RESULTS to view the opening details.`}
+        {` Until it's available, search for ${details} in RESULTS to view the opening details.`}
       </p>
     </Modal>
   );
