ab650c43e159bfdaf0a1ab638681db69b3598587
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2024-11-21 16:25:50 -0800

    chore: making FavoriteButton react to external change

--- frontend/src/components/FavoriteButton/index.tsx
+++ frontend/src/components/FavoriteButton/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button } from '@carbon/react';
 import * as Icons from '@carbon/icons-react';
 import './style.scss'; // Import the styles
@@ -46,6 +46,10 @@ function FavoriteButton({
     return <div>Invalid icon name</div>;
   }
   const CustomIcon = () => <Icon data-testid="favourite-button-icon" style={{ fill }} />;
+
+  useEffect(() => {
+    setIsFavorite(favorited);
+  }, [favorited]);
 
   return (
     <Button
