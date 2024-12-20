0e0a9db825f8950cbca491f6821565a94982b87c
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2024-12-20 06:51:53 -0800

    chore: testing native

--- backend/src/main/java/ca/bc/gov/restapi/results/ResultsApplication.java
+++ backend/src/main/java/ca/bc/gov/restapi/results/ResultsApplication.java
@@ -2,13 +2,15 @@ package ca.bc.gov.restapi.results;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.context.annotation.ComponentScan;
 
-/** Entry point for the RESULTS REST API application. */
+/**
+ * Entry point for the RESULTS REST API application.
+ */
 @SpringBootApplication
-@EnableScheduling
-@EnableAsync
+@ComponentScan("ca.bc.gov.restapi.results")
+@ComponentScan("ca.bc.gov.restapi.results.oracle.entity")
+@ComponentScan("ca.bc.gov.restapi.results.postgres.entity")
 public class ResultsApplication {
 
   public static void main(String[] args) {
