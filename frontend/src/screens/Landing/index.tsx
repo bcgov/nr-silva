7c5e2f0a3e5b5de9a55f9a8aa39c633cfd1ac4c5
Craig Yu <craig.yu93@gmail.com>
2025-01-30 12:30:30 -0800

    fix: bye lottie

--- frontend/src/screens/Landing/index.tsx
+++ frontend/src/screens/Landing/index.tsx
@@ -4,20 +4,10 @@ import { Button } from "@carbon/react";
 import { Login } from '@carbon/icons-react';
 import './Landing.scss';
 import '../../custom.scss';
-import { useLottie } from "lottie-react";
-import silvaLottie from "../../assets/lotties/silva-logo-lottie-1.json"
 import { useGetAuth } from "../../contexts/AuthProvider";
 
 const Landing: React.FC = () => {
-
   const { login } = useGetAuth();
-
-  // Adding the Lottie Loader and loading the View for lottie with initial options
-  const options = {
-    animationData: silvaLottie,
-    loop: true
-  };
-  const { View } = useLottie(options);
 
   return (
     <div className="container-fluid">
@@ -59,14 +49,6 @@ const Landing: React.FC = () => {
             </div>
           </div>
 
-        </div>
-        <div className="col-lg-5">
-          <div className="lottie-container">
-            {View}
-          </div>
-          <div className="text-end fixed-bottom py-2 pe-3">
-            <a href="https://lottiefiles.com/animations/grow-your-forest-DywVyvml06">Animation by Sara Figueroa</a>
-          </div>
         </div>
       </div>
     </div>
