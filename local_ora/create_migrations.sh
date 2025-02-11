6da92946ebaf801e59f64762f8157c2c7f92d446
Kevin Netherton <kevin.netherton@gov.bc.ca>
2025-02-11 10:39:20 -0800

    docs: include docs that describe what was done

---
+++ local_ora/create_migrations.sh
@@ -0,0 +1,182 @@
+uv run python main.py \
+   create-migrations \
+   --seed-table OPEN_CATEGORY_CODE \
+   --schema THE \
+   --migration-folder /home/kjnether/fsa_proj/nr-silva/local_ora/migrations/sql \
+   --migration-name open_cat_cd
+
+uv run python main.py \
+   create-migrations \
+   --seed-table AGE_CLASS_CODE \
+   --schema THE \
+   --migration-folder /home/kjnether/fsa_proj/nr-silva/local_ora/migrations/sql \
+   --migration-name age_cls_cd
+
+uv run python main.py \
+   create-migrations \
+   --seed-table HEIGHT_CLASS_CODE \
+   --schema THE \
+   --migration-folder /home/kjnether/fsa_proj/nr-silva/local_ora/migrations/sql \
+   --migration-name ht_cls_cd
+
+uv run python main.py \
+   create-migrations \
+   --seed-table OPENING_STATUS_CODE \
+   --schema THE \
+   --migration-folder /home/kjnether/fsa_proj/nr-silva/local_ora/migrations/sql \
+   --migration-name open_stat_cd
+
+uv run python main.py \
+   create-migrations \
+   --seed-table STOCKING_CLASS_CODE \
+   --schema THE \
+   --migration-folder /home/kjnether/fsa_proj/nr-silva/local_ora/migrations/sql \
+   --migration-name stl_cls_cd
+
+uv run python main.py \
+   create-migrations \
+   --seed-table SITE_CLASS_CODE \
+   --schema THE \
+   --migration-folder /home/kjnether/fsa_proj/nr-silva/local_ora/migrations/sql \
+   --migration-name site_cls_cd
+
+
+uv run python main.py \
+   create-migrations \
+   --seed-table SITE_INDEX_SOURCE_CODE \
+   --schema THE \
+   --migration-folder /home/kjnether/fsa_proj/nr-silva/local_ora/migrations/sql \
+   --migration-name site_idx_src_cd
+
+uv run python main.py \
+   create-migrations \
+   --seed-table STOCKING_STATUS_CODE \
+   --schema THE \
+   --migration-folder /home/kjnether/fsa_proj/nr-silva/local_ora/migrations/sql \
+   --migration-name stk_stat_cd
+
+uv run python main.py \
+   create-migrations \
+   --seed-table TSB_NUMBER_CODE \
+   --schema THE \
+   --migration-folder /home/kjnether/fsa_proj/nr-silva/local_ora/migrations/sql \
+   --migration-name tsb_num_cd
+
+uv run python main.py \
+   create-migrations \
+   --seed-table TREE_SPECIES_CODE \
+   --schema THE \
+   --migration-folder /home/kjnether/fsa_proj/nr-silva/local_ora/migrations/sql \
+   --migration-name tree_spc_cd
+
+uv run python main.py \
+   create-migrations \
+   --seed-table RESULTS_AUDIT_ACTION_CODE \
+   --schema THE \
+   --migration-folder /home/kjnether/fsa_proj/nr-silva/local_ora/migrations/sql \
+   --migration-name res_aud_act_cd
+
+uv run python main.py \
+   create-migrations \
+   --seed-table CUT_BLOCK_OPEN_ADMIN \
+   --schema THE \
+   --migration-folder /home/kjnether/fsa_proj/nr-silva/local_ora/migrations/sql \
+   --migration-name cut_blk_open_adm
+
+uv run python main.py \
+   create-migrations \
+   --seed-table OPENING_ATTACHMENT \
+   --schema THE \
+   --migration-folder /home/kjnether/fsa_proj/nr-silva/local_ora/migrations/sql \
+   --migration-name open_attach
+
+uv run python main.py \
+   create-migrations \
+   --seed-table OPENING \
+   --schema THE \
+   --migration-folder /home/kjnether/fsa_proj/nr-silva/local_ora/migrations/sql \
+   --migration-name opening
+
+uv run python main.py \
+   create-migrations \
+   --seed-table ORG_UNIT \
+   --schema THE \
+   --migration-folder /home/kjnether/fsa_proj/nr-silva/local_ora/migrations/sql \
+   --migration-name org_unit
+
+uv run python main.py \
+   create-migrations \
+   --seed-table RESULTS_ELECTRONIC_SUBMISSION \
+   --schema THE \
+   --migration-folder /home/kjnether/fsa_proj/nr-silva/local_ora/migrations/sql \
+   --migration-name res_elec_sub
+
+
+uv run python main.py \
+   create-migrations \
+   --seed-table SILV_ADMIN_ZONE \
+   --schema THE \
+   --migration-folder /home/kjnether/fsa_proj/nr-silva/local_ora/migrations/sql \
+   --migration-name silv_adm_zone
+
+
+
+uv run python main.py \
+   create-migrations \
+   --seed-table OPENING_AMENDMENT_HISTORY \
+   --schema THE \
+   --migration-folder /home/kjnether/fsa_proj/nr-silva/local_ora/migrations/sql \
+   --migration-name open_amend_hist
+
+uv run python main.py \
+   create-migrations \
+   --seed-table STOCKING_EVENT_HISTORY \
+   --schema THE \
+   --migration-folder /home/kjnether/fsa_proj/nr-silva/local_ora/migrations/sql \
+   --migration-name stk_event_hist
+
+uv run python main.py \
+   create-migrations \
+   --seed-table RESULTS_AUDIT_EVENT \
+   --schema THE \
+   --migration-folder /home/kjnether/fsa_proj/nr-silva/local_ora/migrations/sql \
+   --migration-name res_aud_evt
+
+uv run python main.py \
+   create-migrations \
+   --seed-table ACTIVITY_TREATMENT_UNIT \
+   --schema THE \
+   --migration-folder /home/kjnether/fsa_proj/nr-silva/local_ora/migrations/sql \
+   --migration-name act_treat_unit
+
+uv run python main.py \
+   create-migrations \
+   --seed-table SILV_RELIEF_APPLICATION \
+   --schema THE \
+   --migration-folder /home/kjnether/fsa_proj/nr-silva/local_ora/migrations/sql \
+   --migration-name silv_rel_app
+
+uv run python main.py \
+   create-migrations \
+   --seed-table STOCKING_STANDARD_UNIT \
+   --schema THE \
+   --migration-folder /home/kjnether/fsa_proj/nr-silva/local_ora/migrations/sql \
+   --migration-name stk_std_unit
+
+uv run python main.py \
+   create-migrations \
+   --seed-table STOCKING_MILESTONE \
+   --schema THE \
+   --migration-folder /home/kjnether/fsa_proj/nr-silva/local_ora/migrations/sql \
+   --migration-name stk_milestone
+
+uv run python main.py \
+   create-migrations \
+   --seed-table FOREST_CLIENT \
+   --schema THE \
+   --migration-folder /home/kjnether/fsa_proj/nr-silva/local_ora/migrations/sql \
+   --migration-name for_clnt
+
+
+
+
