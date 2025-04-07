a4cfa450ce9260317e31eb3cdcd2cbc59caaf958
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2025-04-07 16:29:09 -0700

    chore: removing unwanted stuff

--- backend/src/main/java/ca/bc/gov/restapi/results/oracle/entity/comments/SilvicultureCommentEntity.java
+++ backend/src/main/java/ca/bc/gov/restapi/results/oracle/entity/comments/SilvicultureCommentEntity.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.persistence.Version;
 import jakarta.validation.constraints.Max;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
@@ -13,10 +12,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.With;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Data
@@ -47,23 +42,18 @@ public class SilvicultureCommentEntity {
   private String comment;
 
   @Column(name = "ENTRY_USERID", length = 30)
-  @CreatedBy
   private String entryUserId;
 
   @Column(name = "UPDATE_USERID", length = 30)
-  @LastModifiedBy
   private String updateUserId;
 
   @Column(name = "UPDATE_TIMESTAMP")
-  @LastModifiedDate
   private LocalDateTime updateTimestamp;
 
   @Column(name = "ENTRY_TIMESTAMP")
-  @CreatedDate
   private LocalDateTime entryTimestamp;
 
   @Column(name = "REVISION_COUNT")
-  @Version
   private Long revision;
 
 }
