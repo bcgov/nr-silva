f0948caa2dd488787b86f1f29f88f4b5fa1adb37
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2025-04-07 13:44:01 -0700

    chore: added audit configuration

--- backend/src/main/java/ca/bc/gov/restapi/results/common/configuration/OracleJpaConfiguration.java
+++ backend/src/main/java/ca/bc/gov/restapi/results/common/configuration/OracleJpaConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -28,6 +29,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
     transactionManagerRef = "oracleTransactionManager"
 )
 @EnableTransactionManagement
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 public class OracleJpaConfiguration {
 
   @Bean(name = "oracleEntityManagerFactory")
