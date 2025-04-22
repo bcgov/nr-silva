27dbb22b0670903a8194a6c72dba617ba8aa3903
Shayan Khorsandi <shayan.khorsandi@gov.bc.ca>
2025-04-22 16:46:29 -0700

    feat(699): switch from dummy data to standard unit DTO

--- frontend/src/components/OpeningDetails/OpeningStandardUnits/AccordionTitle.tsx
+++ frontend/src/components/OpeningDetails/OpeningStandardUnits/AccordionTitle.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Location as LocationIcon } from "@carbon/icons-react";
-import { DummyStandardUnitType } from "./constants";
 import VerticalDivider from "../../VerticalDivider";
+import { OpeningDetailsStockingDto } from "@/types/OpeningTypes";
 
 type AcoordionTitleProp = {
-  standardUnit: DummyStandardUnitType;
+  standardUnit: OpeningDetailsStockingDto;
 };
 
 /**
@@ -17,22 +17,22 @@ const AcoordionTitle = ({ standardUnit }: AcoordionTitleProp) => {
       <div className="accordion-title-top">
         <LocationIcon size={20} />
         <h4>
-          {standardUnit.name}
+          {standardUnit.stocking.stockingStandardUnit}
         </h4>
       </div>
       <div className="accordion-title-bottom">
         {/* No standard unit id or FSP id */}
         {
-          (!standardUnit.ssid && !standardUnit.fspId)
+          (!standardUnit.stocking.ssid && !standardUnit.stocking.fspId)
             ? 'Manual stocking requirement'
             : null
         }
         {/* Has standard unit id but no FSP id */}
         {
-          (standardUnit.ssid && !standardUnit.fspId)
+          (standardUnit.stocking.ssid && !standardUnit.stocking.fspId)
             ? (
               <>
-                {`SSID ${standardUnit.ssid}, Stocking objective`}
+                {`SSID ${standardUnit.stocking.ssid}, Stocking objective`}
                 <VerticalDivider />
                 Ministry default
               </>
@@ -41,12 +41,12 @@ const AcoordionTitle = ({ standardUnit }: AcoordionTitleProp) => {
         }
         {/* Has standard unit id AND FSP id */}
         {
-          (standardUnit.ssid && standardUnit.fspId)
+          (standardUnit.stocking.ssid && standardUnit.stocking.fspId)
             ? (
               <>
-                {`SSID ${standardUnit.ssid}, Stocking objective`}
+                {`SSID ${standardUnit.stocking.ssid}, Stocking objective`}
                 <VerticalDivider />
-                {`FSP ID ${standardUnit.fspId}`}
+                {`FSP ID ${standardUnit.stocking.fspId}`}
               </>
             )
             : null
