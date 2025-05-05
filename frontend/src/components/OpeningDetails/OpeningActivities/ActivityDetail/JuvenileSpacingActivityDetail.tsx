1a17b3e4b8e9162f9fb7d7a0456a691b8be5235a
Shayan Khorsandi <shayan.khorsandi@gov.bc.ca>
2025-05-05 09:16:12 -0700

    feat(711): add juvenile spacing activity detail fields

--- frontend/src/components/OpeningDetails/OpeningActivities/ActivityDetail/JuvenileSpacingActivityDetail.tsx
+++ frontend/src/components/OpeningDetails/OpeningActivities/ActivityDetail/JuvenileSpacingActivityDetail.tsx
@@ -1,7 +1,45 @@
+import { Column, Grid, SkeletonText } from "@carbon/react";
 import { ActivityDetailProps, MockedActivityDetailType } from "../definitions";
+import { CardItem } from "../../../Card";
 
 const JuvenileSpacingActivityDetail = ({ activityDetail, isLoading }: ActivityDetailProps) => {
-    return (<div>Juvenile Spacing Detail</div>);
+    return (
+        <Grid className="expanded-row-content-grid">
+            <Column sm={4} md={4} lg={16}>
+                {isLoading ? (
+                    <SkeletonText className="expanded-row-title" />
+                ) : (
+                    <h6 className="expanded-row-title">
+                        Spacing Specifications
+                    </h6>
+                )}
+            </Column>
+
+            <Column sm={4} md={4} lg={4}>
+                <CardItem label="Target Inter-tree distance(m)" showSkeleton={isLoading}>
+                    {activityDetail?.spacingSpecification?.targetInterTreeDistance}
+                </CardItem>
+            </Column>
+
+            <Column sm={4} md={4} lg={4}>
+                <CardItem label="Allowable variation in inter-tree distance(m)" showSkeleton={isLoading}>
+                    {activityDetail?.spacingSpecification?.allowableVariationInterTreeDistance}
+                </CardItem>
+            </Column>
+
+            <Column sm={4} md={4} lg={4}>
+                <CardItem label="Allowable trees per plot" showSkeleton={isLoading}>
+                    {activityDetail?.spacingSpecification?.allowableTreesPerPlot}
+                </CardItem>
+            </Column>
+
+            <Column sm={4} md={4} lg={4}>
+                <CardItem label="Spacing per ha" showSkeleton={isLoading}>
+                    {activityDetail?.spacingSpecification?.spacingPerHa}
+                </CardItem>
+            </Column>
+        </Grid>
+    );
 };
 
 export default JuvenileSpacingActivityDetail;
