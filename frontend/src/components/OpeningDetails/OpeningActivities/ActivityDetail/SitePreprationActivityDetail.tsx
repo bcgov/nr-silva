1bc0c4bde9e28f7d7a97d03edf5a4623bad13bae
Shayan Khorsandi <shayan.khorsandi@gov.bc.ca>
2025-05-05 09:55:30 -0700

    feat(711): add site prep activity detail fields

--- frontend/src/components/OpeningDetails/OpeningActivities/ActivityDetail/SitePreprationActivityDetail.tsx
+++ frontend/src/components/OpeningDetails/OpeningActivities/ActivityDetail/SitePreprationActivityDetail.tsx
@@ -1,7 +1,27 @@
+import { Column, Grid, SkeletonText } from "@carbon/react";
 import { ActivityDetailProps, MockedActivityDetailType } from "../definitions";
+import { CardItem } from "../../../Card";
 
 const SitePreparationActivityDetail = ({ activityDetail, isLoading }: ActivityDetailProps) => {
-    return (<div>Site Prepration Activity Detail</div>);
+    return (
+        <Grid className="expanded-row-content-grid">
+            <Column sm={4} md={4} lg={16}>
+                {isLoading ? (
+                    <SkeletonText className="expanded-row-title" />
+                ) : (
+                    <h6 className="expanded-row-title">
+                        Site prepration specifications
+                    </h6>
+                )}
+            </Column>
+
+        <Column sm={16} md={16} lg={16}>
+            <CardItem label="Target prepared spot per ha" showSkeleton={isLoading}>
+                {activityDetail?.sitePrepSpecification?.targetPreparedSpotPerHa}
+            </CardItem>
+        </Column>
+    </Grid>
+    );
 };
 
 export default SitePreparationActivityDetail;
