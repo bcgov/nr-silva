2426e364b0b5057bd8e4cd1feccd5058ef6b5405
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2025-05-13 16:39:12 -0700

    feat(#648): updating map entry
    Updated component to make use of the geojson instead of parsing and reparsing it.

---
+++ frontend/src/components/OpeningsMapEntry/constants.ts
@@ -0,0 +1,7 @@
+export const resultsStyle = {
+  fillColor: "#4A90E2", // A vibrant but professional blue
+  color: "#2C3E50", // Darker stroke for contrast
+  weight: 2, // Thin stroke for clarity
+  opacity: 1, // Full opacity for strong visibility
+  fillOpacity: 0.6, // Slight transparency to blend with the map
+};
--- frontend/src/components/OpeningsMapEntry/index.tsx
+++ frontend/src/components/OpeningsMapEntry/index.tsx
@@ -1,53 +1,71 @@
-import React, { useEffect } from 'react';
-import { Polygon, useMap, Popup } from 'react-leaflet';
-import L, { LatLngExpression } from 'leaflet';
-import { OpeningPolygon } from '../../types/OpeningPolygon';
-import OpeningsMapEntryPopup from '../OpeningsMapEntryPopup';
+import React, { useEffect, useState } from "react";
+import { useMap, Popup, GeoJSON } from "react-leaflet";
+import { FeatureCollection } from "geojson";
+import OpeningsMapEntryPopup from "@/components/OpeningsMapEntryPopup";
+import { resultsStyle } from "./constants";
 
 interface OpeningsMapEntryProps {
-  polygons: OpeningPolygon[];
-  defaultLocation: LatLngExpression;
-  defaultZoom: number;
+  polygons: FeatureCollection[];
 }
 
-const resultsStyle = {
-  color: 'black'
-};
+const OpeningsMapEntry: React.FC<OpeningsMapEntryProps> = ({ polygons }) => {
+  // State to hold the polygons
+  // This is used to set the map view when the component mounts
+  const [features, setFeatures] = useState<FeatureCollection[]>([]);
 
-const OpeningsMapEntry: React.FC<OpeningsMapEntryProps> = ({
-  polygons,
-  defaultLocation,
-  defaultZoom
-}) => {
+  // Get the map instance from react-leaflet
   const map = useMap();
 
-  useEffect(() => {
-    if (polygons.length > 0) {
-      // Calculate bounds to include all polygons
-      // This is to make sure the map resizes for the correct view
-      const bounds = L.latLngBounds(polygons.flatMap((polygon) => polygon.bounds.flat()).filter((point): point is LatLngExpression => point !== undefined));
-      map.fitBounds(bounds, { padding: [20, 20] });
-    } else {
-      // If no polygons are found, set the map to the default location
-      map.setView(defaultLocation, defaultZoom);
-    }
-  }, [polygons, map]);
+  /**
+   * Function to generate a unique key for each polygon
+   * This is used to avoid duplicate keys in the map and allow React to
+   * efficiently update the map when the polygons change
+   * @param polygon The polygon to generate the key for
+   * @param index The index of the polygon in the array
+   * @returns A unique key for the polygon
+   */
+  const geoKey = (polygon: FeatureCollection, index: number) =>
+    polygon.features
+      .filter(Boolean)
+      .map((feature) => feature.id)
+      .filter(Boolean)
+      .map((id) => String(id))
+      .reduce((acc, id) => id, "") ?? `geo-${index}`;
 
+  /**
+   * Function to get the opening ID from the polygon
+   * This is used to display the opening ID in the popup when a polygon is clicked
+   * @param polygon The polygon to get the opening ID from
+   * @returns The opening ID of the polygon or 0 if not found
+   */
+  const getOpeningId = (polygon: FeatureCollection) =>
+    polygon.features
+      .filter(Boolean)
+      .map((feature) => feature.properties?.OPENING_ID)
+      .filter(Boolean)
+      .map((id) => String(id))
+      .map((id) => parseFloat(id))
+      .reduce((acc, id) => id, 0) ?? 0;
+
+  /**
+   * Effect to set the map view when the component mounts or when the polygons change
+   */
+  useEffect(() => {
+    setFeatures([...polygons]);
+  }, [polygons, map]);
 
   return (
     <>
-      {polygons.map((polygon) => (
-        <Polygon
-          key={polygon.key}
-          positions={polygon.bounds}
-          pathOptions={resultsStyle}
+      {features.filter(Boolean).map((polygon, index) => (
+        <GeoJSON
+          data={polygon}
+          key={geoKey(polygon, index)}
+          style={resultsStyle}
         >
           <Popup maxWidth={700}>
-            <OpeningsMapEntryPopup
-              openingId={parseFloat((polygon.properties?.OPENING_ID as string) || '0')}
-            />
+            <OpeningsMapEntryPopup openingId={getOpeningId(polygon)} />
           </Popup>
-        </Polygon>
+        </GeoJSON>
       ))}
     </>
   );
