95b43e51fa2e1d1998657fa54a5fccc48a346a08
Craig Yu <craig.yu93@gmail.com>
2025-05-15 14:35:13 -0700

    fix: formatActivityObjective

--- frontend/src/components/OpeningDetails/OpeningActivities/utils.ts
+++ frontend/src/components/OpeningDetails/OpeningActivities/utils.ts
@@ -1,11 +1,11 @@
 import { PLACE_HOLDER, UNIQUE_CHARACTERS_UNICODE } from "@/constants";
-import CodeDescriptionDto from "@/types/CodeDescriptionType";
 import { OpeningDetailsActivitiesActivitiesDto } from "@/types/OpeningTypes";
+import { codeDescriptionToDisplayText, extractCodesFromCodeDescriptionArr } from "@/utils/multiSelectUtils";
 
 /**
  * Generates tooltip definitions and display text for objectives.
  *
- * This function processes up to three objectives (`objective1`, `objective2`, `objective3`) 
+ * This function processes up to three objectives (`objective1`, `objective2`, `objective3`)
  * from the provided data object and returns a formatted tooltip definition and display text.
  *
  * @param data - The data object containing objectives. Expected to conform to the `OpeningDetailsActivitiesActivitiesDto` type.
@@ -16,28 +16,24 @@ import { OpeningDetailsActivitiesActivitiesDto } from "@/types/OpeningTypes";
 export const formatActivityObjective = (
   data: OpeningDetailsActivitiesActivitiesDto | null,
 ): { tooltipDefinition: string[] | null; displayText: string } => {
-  const objective1 = data?.objective1 as CodeDescriptionDto;
-  const objective2 = data?.objective2 as CodeDescriptionDto;
-  const objective3 = data?.objective3 as CodeDescriptionDto;
-
-  const tooltipDefinition = [
-    (objective1?.code && objective1?.description) ? `${objective1.code} - ${objective1.description}` : null,
-    (objective2?.code && objective2?.description) ? `${objective2.code} - ${objective2.description}` : null,
-    (objective3?.code && objective3?.description) ? `${objective3.code} - ${objective3.description}` : null,
-  ].filter((objective) => objective !== null);
-
-  const displayText = [
-    objective1?.code ?? false,
-    objective2?.code ?? false,
-    objective3?.code ?? false
-  ].filter(Boolean).join(` ${UNIQUE_CHARACTERS_UNICODE.BULLET} `);
-
-  if (displayText.length === 0) {
+  if (!data) {
     return {
       tooltipDefinition: null,
-      displayText: PLACE_HOLDER,
-    };
+      displayText: PLACE_HOLDER
+    }
   }
+
+  const objectives = [data.objective1, data.objective2, data.objective3].filter(Boolean);
+
+  const tooltipDefinition: string[] = objectives
+    .filter((obj) => codeDescriptionToDisplayText(obj) !== PLACE_HOLDER)
+    .map((obj) => codeDescriptionToDisplayText(obj));
+
+  let displayText = extractCodesFromCodeDescriptionArr(objectives)
+    .filter((code) => code != null && code !== '')
+    .join(` ${UNIQUE_CHARACTERS_UNICODE.BULLET} `);
+
+  displayText = displayText.length ? displayText : PLACE_HOLDER
 
   return {
     tooltipDefinition,
