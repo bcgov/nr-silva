1d953d922ee93561aa385baea35373a318841a3a
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2025-05-21 11:50:49 -0700

    feat(#648): updating map (#780)

--- frontend/src/components/OpeningsMapEntry/index.tsx
+++ frontend/src/components/OpeningsMapEntry/index.tsx
@@ -1,54 +1,132 @@
-import React, { useEffect } from 'react';
-import { Polygon, useMap, Popup } from 'react-leaflet';
-import L, { LatLngExpression } from 'leaflet';
-import { OpeningPolygon } from '../../types/OpeningPolygon';
-import OpeningsMapEntryPopup from '../OpeningsMapEntryPopup';
+import React, { useEffect, useState } from "react";
+import { useMapEvents, Popup, GeoJSON, Marker } from "react-leaflet";
+import L from "leaflet";
+import { FeatureCollection } from "geojson";
+import {
+  getStyleForFeature,
+  getPropertyForFeature,
+  getPopupCenter,
+  getCenterOfFeatureCollection,
+} from "@/types/MapLayer";
+import OpeningsMapEntryPopup from "@/components/OpeningsMapEntryPopup";
+
+import "./styles.scss";
 
 interface OpeningsMapEntryProps {
-  polygons: OpeningPolygon[];
-  defaultLocation: LatLngExpression;
-  defaultZoom: number;
+  polygons: FeatureCollection[];
 }
 
-const resultsStyle = {
-  color: 'black'
-};
+const OpeningsMapEntry: React.FC<OpeningsMapEntryProps> = ({ polygons }) => {
+  // State to hold the polygons
+  // This is used to set the map view when the component mounts
+  const [features, setFeatures] = useState<FeatureCollection[]>([]);
+  const [zoom, setZoom] = useState<number>(13);
 
-const OpeningsMapEntry: React.FC<OpeningsMapEntryProps> = ({
-  polygons,
-  defaultLocation,
-  defaultZoom
-}) => {
-  const map = useMap();
+  // Get the map instance from react-leaflet
+  const map = useMapEvents({
+    zoomend: () => setZoom(map.getZoom()),
+  });
 
+  const markerIcon = new L.Icon({
+    iconUrl: "/marker.svg",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
+  });
+
+  /**
+   * Function to generate a unique key for each polygon
+   * This is used to avoid duplicate keys in the map and allow React to
+   * efficiently update the map when the polygons change
+   * @param polygon The polygon to generate the key for
+   * @param index The index of the polygon in the array
+   * @returns A unique key for the polygon
+   */
+  const geoKey = (polygon: FeatureCollection, index: number) =>
+    polygon.features
+      .filter(Boolean)
+      .map((feature) => feature.id)
+      .filter(Boolean)
+      .map((id) => String(id))
+      .reduce((acc, id) => id, "") ?? `geo-${index}`;
+
+  /**
+   * Function to get the opening ID from the polygon
+   * This is used to display the opening ID in the popup when a polygon is clicked
+   * @param polygon The polygon to get the opening ID from
+   * @returns The opening ID of the polygon or 0 if not found
+   */
+  const getOpeningId = (polygon: FeatureCollection) =>
+    polygon.features
+      .filter(Boolean)
+      .map((feature) => feature.properties?.OPENING_ID)
+      .filter(Boolean)
+      .map((id) => String(id))
+      .map((id) => parseFloat(id))
+      .find((id) => id && id !== 0) ?? 0;
+
+  /**
+   * Effect to set the map view when the component mounts or when the polygons change
+   */
   useEffect(() => {
-    if (polygons.length > 0) {
-      // Calculate bounds to include all polygons
-      // This is to make sure the map resizes for the correct view
-      const bounds = L.latLngBounds(polygons.flatMap((polygon) => polygon.bounds.flat()).filter((point): point is LatLngExpression => point !== undefined));
-      map.fitBounds(bounds, { padding: [20, 20] });
-    } else {
-      // If no polygons are found, set the map to the default location
-      map.setView(defaultLocation, defaultZoom);
-    }
-  }, [polygons, map]);
-
+    setFeatures([...polygons]);
+  }, [polygons]);
 
   return (
     <>
-      {polygons.map((polygon) => (
-        <Polygon
-          key={polygon.key}
-          positions={polygon.bounds}
-          pathOptions={resultsStyle}
-        >
-          <Popup maxWidth={700}>
-            <OpeningsMapEntryPopup
-              openingId={parseFloat((polygon.properties?.OPENING_ID as string) || '0')}
-            />
-          </Popup>
-        </Polygon>
-      ))}
+      {zoom > 10 &&
+        features.filter(Boolean).map((featureCollection, index) => (
+          <GeoJSON
+            data-testid="geojson"
+            data={featureCollection}
+            key={geoKey(featureCollection, index)}
+            style={getStyleForFeature}
+          >
+            {featureCollection?.features
+              ?.filter((feature) => feature.geometry)
+              .map((feature, index) => (
+                <Popup
+                  data-testid="popup"
+                  key={`popup-${geoKey(featureCollection, index)}-geo`}
+                  maxWidth={700}
+                  autoPan={false}
+                  position={getPopupCenter(feature.geometry)}
+                >
+                  <OpeningsMapEntryPopup
+                    openingId={getOpeningId(featureCollection)}
+                    data={getPropertyForFeature(feature)}
+                  />
+                </Popup>
+              ))}
+          </GeoJSON>
+        ))}
+      {zoom <= 10 &&
+        features.filter(Boolean).map((featureCollection, index) => (
+          <Marker
+            icon={markerIcon}
+            data-testid="marker"
+            key={geoKey(featureCollection, index)}
+            position={getCenterOfFeatureCollection(featureCollection)}
+          >
+            {featureCollection?.features
+              ?.filter((feature) => feature.geometry)
+              .map((feature, index) => (
+                <Popup
+                  data-testid="popup"
+                  key={`popup-${geoKey(featureCollection, index)}-marker`}
+                  maxWidth={700}
+                  autoPan={false}
+                  position={getPopupCenter(feature.geometry)}
+                >
+                  <OpeningsMapEntryPopup
+                    openingId={getOpeningId(featureCollection)}
+                    data={getPropertyForFeature(feature)}
+                  />
+                </Popup>
+              ))}
+          </Marker>
+        ))}
     </>
   );
 };
---
+++ frontend/src/components/OpeningsMapFitBound/index.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from "react";
+import { useMap } from "react-leaflet";
+import L, { LatLngExpression } from "leaflet";
+import { FeatureCollection } from "geojson";
+
+interface OpeningsMapFitBoundProps {
+  polygons: FeatureCollection[];
+  defaultLocation: LatLngExpression;
+  defaultZoom: number;
+}
+
+const OpeningsMapFitBound: React.FC<OpeningsMapFitBoundProps> = ({
+  polygons,
+  defaultLocation,
+  defaultZoom,
+}) => {
+  const map = useMap();
+
+  /**
+   * Effect to set the map view when the component mounts or when the polygons change
+   * This is used to ensure that the map is centered on the polygons
+   * and that the map is resized to fit the polygons
+   * If no polygons are found, set the map to the default location
+   * If polygons are found, calculate the bounds to include all polygons
+   * and set the map view to the bounds
+   * If the bounds are not valid, set the map to the default location
+   */
+  useEffect(() => {
+    if (polygons.length > 0) {
+      // Calculate bounds to include all polygons
+      // This is to make sure the map resizes for the correct view
+      const bounds = L.featureGroup(
+        polygons.map((geoJson) => L.geoJSON(geoJson))
+      ).getBounds();
+
+      if (bounds.isValid()) map.fitBounds(bounds, { padding: [20, 20] });
+      else map.setView(defaultLocation, defaultZoom);
+    } else {
+      // If no polygons are found, set the map to the default location
+      map.setView(defaultLocation, defaultZoom);
+    }
+  }, [polygons, map]);
+
+  return null;
+};
+
+export default OpeningsMapFitBound;
