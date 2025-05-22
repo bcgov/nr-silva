fcbade571265a62ff389a148cfdaf33245348603
Shayan Khorsandi <shayan.khorsandi@gov.bc.ca>
2025-05-22 14:19:57 -0700

    feat(#795): render place holder when comments array is empty

--- frontend/src/components/Comments/index.tsx
+++ frontend/src/components/Comments/index.tsx
@@ -1,23 +1,26 @@
 import { CommentDto } from "@/types/CommentTypes";
+import { PLACE_HOLDER } from "@/constants";
 
 interface CommentProps {
-    comments: CommentDto[];
+  comments: CommentDto[];
 }
 
-const Comments = ({comments}: CommentProps) => {
-    return (
-        <ul className="comment-list">
-            {
-                comments.map((comment, index) =>
-                    comment.commentText ? (
-                        <li key={index}>
-                            {comment.commentText}
-                        </li>
-                    ) : null
-                )
-            }
-        </ul>
-    );
+const Comments = ({ comments }: CommentProps) => {
+  return (
+    comments?.length ? (
+      <ul className="comment-list">
+        {
+          comments.map((comment, index) =>
+            comment.commentText ? (
+              <li key={index}>
+                {comment.commentText}
+              </li>
+            ) : null
+          )
+        }
+      </ul>
+    ) : PLACE_HOLDER
+  );
 }
 
 export default Comments;
