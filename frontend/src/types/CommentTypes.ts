23629c9aeb42c1e23ead8fd6371cbcbfece47cf8
Shayan Khorsandi <shayan.khorsandi@gov.bc.ca>
2025-05-28 16:06:45 -0700

    feat(#799): cleanup manually defined types and use OpenAPI types

--- frontend/src/types/CommentTypes.ts
+++
@@ -1,7 +0,0 @@
-import CodeDescriptionDto from "./CodeDescriptionType"
-
-export type CommentDto = {
-  commentSource: CodeDescriptionDto,
-  codeDescriptionDto: CodeDescriptionDto,
-  commentText: string | null
-}
