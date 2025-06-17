6cfc0bcef3e55ad30d82b48753531a22dc50be8c
Shayan Khorsandi <shayan.khorsandi@gov.bc.ca>
2025-06-17 08:47:34 -0700

    feat(#854): add description and NR status to milestone type

--- backend/src/main/java/ca/bc/gov/restapi/results/oracle/enums/StockingMilestoneTypeEnum.java
+++ backend/src/main/java/ca/bc/gov/restapi/results/oracle/enums/StockingMilestoneTypeEnum.java
@@ -1,17 +1,24 @@
 package ca.bc.gov.restapi.results.oracle.enums;
 
 public enum StockingMilestoneTypeEnum {
-    FG("FG"),
-    RG("RG"),
-    PH("PH");
+    FG("FG", "Free Growing"),
+    RG("RG", "Regeneration"),
+    PH("PH", "Post Harvest"),
+    NR("NR", "No Regeneration");
 
     private final String code;
+    private final String description;
 
-    StockingMilestoneTypeEnum(String code) {
+    StockingMilestoneTypeEnum(String code, String description) {
         this.code = code;
+        this.description = description;
     }
 
     public String getCode() {
         return code;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
