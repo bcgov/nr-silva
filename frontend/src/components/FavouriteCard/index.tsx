32d2920c096903d0ca2a56e241b278202a4abb11
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2025-06-18 12:30:02 -0700

    test: remove unused description property from FavouriteCard component

--- frontend/src/components/FavouriteCard/index.tsx
+++ frontend/src/components/FavouriteCard/index.tsx
@@ -13,7 +13,6 @@ import "./styles.scss";
  * @property {string} title - The title of the card.
  * @property {string} link - The link to navigate to when the card is clicked.
  * @property {string} icon - The name of the Carbon icon to display on the card.
- * @property {string} description - The description of the card.
  * @returns {React.ReactElement} The rendered card component.
  */
 function FavouriteCard({
