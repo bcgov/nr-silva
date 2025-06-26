4451e10515d0782090a07d28ca749e7048e1acb1
Paulo Gomes da Cruz Junior <paulo.cruz@gov.bc.ca>
2025-06-26 14:03:32 -0700

    feat(#878): refactor GrantedAuthoritiesConverter to use cognito:groups for role extraction

--- backend/src/main/java/ca/bc/gov/restapi/results/common/security/GrantedAuthoritiesConverter.java
+++ backend/src/main/java/ca/bc/gov/restapi/results/common/security/GrantedAuthoritiesConverter.java
@@ -1,8 +1,10 @@
 package ca.bc.gov.restapi.results.common.security;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -15,19 +17,17 @@ public class GrantedAuthoritiesConverter implements Converter<Jwt, Collection<Gr
 
   @Override
   public Collection<GrantedAuthority> convert(Jwt jwt) {
-    final List<String> realmAccess = new ArrayList<>();
-    Object clientRolesObj =
-        jwt
-        .getClaims()
-        .getOrDefault("client_roles", List.of());
-
-    if (clientRolesObj instanceof List<?> list) {
-      list.forEach(item -> realmAccess.add(String.valueOf(item)));
-    }
-    return realmAccess
-        .stream()
-        .map(roleName -> "ROLE_" + roleName)
-        .map(roleName -> (GrantedAuthority) new SimpleGrantedAuthority(roleName))
-        .toList();
+      return
+          Optional
+              .ofNullable(
+                  jwt.getClaimAsStringList("cognito:groups")
+              )
+              .orElse(List.of())
+              .stream()
+              .filter(Objects::nonNull)
+              .map(Object::toString)
+              .map(roleName -> "ROLE_" + roleName)
+              .map(roleName -> (GrantedAuthority) new SimpleGrantedAuthority(roleName))
+              .collect(Collectors.toSet());
   }
 }
