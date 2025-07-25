81f1f113bbd76ae74f33c082b2ec1bab207417d7
Shayan Khorsandi <shayan.khorsandi@gov.bc.ca>
2025-07-25 12:05:46 -0700

    feat(#900): add forest cover history projections

---
+++ backend/src/main/java/ca/bc/gov/restapi/results/oracle/entity/cover/history/ForestCoverHistoryDetailsLayerProjection.java
@@ -0,0 +1,6 @@
+package ca.bc.gov.restapi.results.oracle.entity.cover.history;
+
+import ca.bc.gov.restapi.results.oracle.entity.cover.ForestCoverDetailsLayerProjection;
+
+public interface ForestCoverHistoryDetailsLayerProjection extends ForestCoverDetailsLayerProjection {
+}
