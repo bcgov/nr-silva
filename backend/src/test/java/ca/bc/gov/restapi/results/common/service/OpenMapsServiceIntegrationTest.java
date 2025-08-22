9b5ead8569e66567f0ce202a6b70a68b08c8d082
Shayan Khorsandi <shayan.khorsandi@gov.bc.ca>
2025-08-22 12:39:34 -0700

    fix(#977): fix property name order and test parameter in OpenMapsServiceIntegrationTest
    Corrects the order of PROPERTYNAME query parameter and updates the test to use the correct layer name in getOpeningPolygonAndProperties. Ensures test accuracy and alignment with expected API behavior.

--- backend/src/test/java/ca/bc/gov/restapi/results/common/service/OpenMapsServiceIntegrationTest.java
+++ backend/src/test/java/ca/bc/gov/restapi/results/common/service/OpenMapsServiceIntegrationTest.java
@@ -52,7 +52,7 @@ class OpenMapsServiceIntegrationTest {
             .withQueryParam("typeName", equalTo("WHSE_FOREST_VEGETATION.RSLT_OPENING_SVW"))
             .withQueryParam("outputFormat", equalTo("application/json"))
             .withQueryParam("SrsName", equalTo("EPSG:4326"))
-            .withQueryParam("PROPERTYNAME", equalTo("GEOMETRY,OPENING_ID"))
+            .withQueryParam("PROPERTYNAME", equalTo("OPENING_ID,GEOMETRY"))
             .withQueryParam("CQL_FILTER", equalTo("OPENING_ID=" + openingId))
             .willReturn(okJson(TestConstants.WFS_OPENING))
     );
@@ -113,7 +113,7 @@ class OpenMapsServiceIntegrationTest {
             .willReturn(okJson(TestConstants.FTEN_CUT_BLOCK_POLY_SVW))
     );
 
-    FeatureCollection response = openMapsService.getOpeningPolygonAndProperties(openingId, TestConstants.FTEN_CUT_BLOCK_POLY_SVW);
+    FeatureCollection response = openMapsService.getOpeningPolygonAndProperties(openingId, "WHSE_FOREST_TENURE.FTEN_CUT_BLOCK_POLY_SVW");
 
     Assertions.assertNotNull(response);
     Assertions.assertFalse(response.getFeatures().isEmpty());
