b936d1dc0869404f163c6d3b96411b47e98a66ce
Shayan Khorsandi <shayan.khorsandi@gov.bc.ca>
2025-09-05 16:58:01 -0700

    feat(#972): add opening stocking standard history details projections

---
+++ backend/src/main/java/ca/bc/gov/restapi/results/oracle/entity/opening/history/OpeningStockingSpeciesHistoryProjection.java
@@ -0,0 +1,6 @@
+package ca.bc.gov.restapi.results.oracle.entity.opening.history;
+
+import ca.bc.gov.restapi.results.oracle.entity.opening.OpeningStockingSpeciesProjection;
+
+public interface OpeningStockingSpeciesHistoryProjection extends OpeningStockingSpeciesProjection {
+}
