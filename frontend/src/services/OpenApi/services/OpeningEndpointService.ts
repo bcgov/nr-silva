b201328539810006c988732c649081113a1cbd47
Shayan Khorsandi <shayan.khorsandi@gov.bc.ca>
2025-09-18 10:58:19 -0700

    feat(#971): add mainSearchTerm to getCoverHistory method
    The getCoverHistory method in OpeningEndpointService now accepts an optional mainSearchTerm parameter, which is included in the query parameters for the API request to filter data.

--- frontend/src/services/OpenApi/services/OpeningEndpointService.ts
+++ frontend/src/services/OpenApi/services/OpeningEndpointService.ts
@@ -188,12 +188,14 @@ export class OpeningEndpointService {
     /**
      * @param openingId
      * @param updateDate
+     * @param mainSearchTerm
      * @returns OpeningForestCoverHistoryDto OK
      * @throws ApiError
      */
     public static getCoverHistory(
         openingId: number,
         updateDate: string,
+        mainSearchTerm?: string,
     ): CancelablePromise<Array<OpeningForestCoverHistoryDto>> {
         return __request(OpenAPI, {
             method: 'GET',
@@ -203,6 +205,7 @@ export class OpeningEndpointService {
             },
             query: {
                 'updateDate': updateDate,
+                'mainSearchTerm': mainSearchTerm,
             },
         });
     }
