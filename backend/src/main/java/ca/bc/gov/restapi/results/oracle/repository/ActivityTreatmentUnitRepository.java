bd5b7d9cc4ecce857dadb236c5988476db043621
Craig Yu <craig.yu93@gmail.com>
2025-11-12 12:54:01 -0800

    fix: status logic

--- backend/src/main/java/ca/bc/gov/restapi/results/oracle/repository/ActivityTreatmentUnitRepository.java
+++ backend/src/main/java/ca/bc/gov/restapi/results/oracle/repository/ActivityTreatmentUnitRepository.java
@@ -16,22 +16,20 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface ActivityTreatmentUnitRepository extends
-    JpaRepository<ActivityTreatmentUnitEntity, Long> {
+public interface ActivityTreatmentUnitRepository
+    extends JpaRepository<ActivityTreatmentUnitEntity, Long> {
 
   @Query(
       nativeQuery = true,
       value = SilvaOracleQueryConstants.GET_OPENING_ACTIVITIES_DISTURBANCE,
-      countQuery = SilvaOracleQueryConstants.GET_OPENING_ACTIVITIES_DISTURBANCE_COUNT
-  )
+      countQuery = SilvaOracleQueryConstants.GET_OPENING_ACTIVITIES_DISTURBANCE_COUNT)
   Page<OpeningActivitiesDisturbanceProjection> getOpeningActivitiesDisturbanceByOpeningId(
       Long openingId, Pageable page);
 
   @Query(
       nativeQuery = true,
       value = SilvaOracleQueryConstants.GET_OPENING_ACTIVITIES_ACTIVITIES,
-      countQuery = SilvaOracleQueryConstants.GET_OPENING_ACTIVITIES_ACTIVITIES_COUNT
-  )
+      countQuery = SilvaOracleQueryConstants.GET_OPENING_ACTIVITIES_ACTIVITIES_COUNT)
   Page<OpeningActivitiesActivitiesProjection> getOpeningActivitiesActivitiesByOpeningId(
       Long openingId, String mainSearchTerm, Pageable page);
 
@@ -52,5 +50,4 @@ public interface ActivityTreatmentUnitRepository extends
 
   @Query(nativeQuery = true, value = SilvaOracleQueryConstants.GET_OPENING_ACTIVITY_SP)
   Optional<Long> getOpeningActivitySP(Long openingId, Long atuId);
-
 }
