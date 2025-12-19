ed032b34e31d432916cb5fbc4cc50b4b59ea9987
Shayan Khorsandi <shayan.khorsandi@gov.bc.ca>
2025-12-18 16:58:15 -0800

    feat(#1107): add Flyway configuration customizer for environment-based migrations
    Introduces a FlywayConfiguration class to customize migration script locations based on the FLYWAY_ENVIRONMENT variable. Removes static Flyway locations from application.yml to allow dynamic configuration for different environments.

---
+++ backend/src/main/java/ca/bc/gov/restapi/results/common/configuration/FlywayConfiguration.java
@@ -0,0 +1,22 @@
+package ca.bc.gov.restapi.results.common.configuration;
+
+import org.springframework.boot.autoconfigure.flyway.FlywayConfigurationCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class FlywayConfiguration {
+
+  @Bean
+  public FlywayConfigurationCustomizer flywayConfigurationCustomizer(Environment env) {
+    return configuration -> {
+      String environmentName = env.getProperty("FLYWAY_ENVIRONMENT", "prod");
+      if (environmentName.equals("prod")) {
+        configuration.locations("classpath:db/migration");
+      } else {
+        configuration.locations("classpath:db/migration", "classpath:db/migration-dev");
+      }
+    };
+  }
+}
