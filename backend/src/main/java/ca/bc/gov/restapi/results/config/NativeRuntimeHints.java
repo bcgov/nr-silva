2945c00ecba3a37cf0c6cd252bdffe8850d5f9ae
Craig Yu <craig.yu93@gmail.com>
2026-01-05 13:56:03 -0800

    Register JPA entities for reflection in native builds
    Added Oracle and Postgres entity classes to the GraalVM native image runtime hints for reflection. This ensures proper proxy creation and entity access in multi-datasource native builds.

--- backend/src/main/java/ca/bc/gov/restapi/results/config/NativeRuntimeHints.java
+++ backend/src/main/java/ca/bc/gov/restapi/results/config/NativeRuntimeHints.java
@@ -1,13 +1,26 @@
 package ca.bc.gov.restapi.results.config;
 
+import ca.bc.gov.restapi.results.oracle.entity.ClientAcronymEntity;
+import ca.bc.gov.restapi.results.oracle.entity.CutBlockOpenAdminEntity;
+import ca.bc.gov.restapi.results.oracle.entity.OpenCategoryCodeEntity;
+import ca.bc.gov.restapi.results.oracle.entity.OrgUnitEntity;
+import ca.bc.gov.restapi.results.oracle.entity.ResultsElectronicSubmissionEntity;
+import ca.bc.gov.restapi.results.oracle.entity.activities.ActivityTreatmentUnitEntity;
+import ca.bc.gov.restapi.results.oracle.entity.comments.SilvicultureCommentEntity;
+import ca.bc.gov.restapi.results.oracle.entity.cover.ForestCoverEntity;
+import ca.bc.gov.restapi.results.oracle.entity.opening.OpeningAttachmentEntity;
+import ca.bc.gov.restapi.results.oracle.entity.opening.OpeningEntity;
+import ca.bc.gov.restapi.results.postgres.entity.UserOpeningEntity;
+import ca.bc.gov.restapi.results.postgres.entity.UserRecentOpeningEntity;
 import jakarta.persistence.EntityManagerFactory;
+import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.orm.jpa.EntityManagerFactoryInfo;
 
 /**
  * Registers runtime hints for GraalVM native image compilation.
- * This is required for JPA EntityManagerFactory proxy creation.
+ * This is required for JPA EntityManagerFactory proxy creation and entity reflection.
  */
 public class NativeRuntimeHints implements RuntimeHintsRegistrar {
 
@@ -18,5 +31,40 @@ public class NativeRuntimeHints implements RuntimeHintsRegistrar {
         EntityManagerFactory.class,
         EntityManagerFactoryInfo.class
     );
+
+    // Register all Oracle entities for reflection (required for multi-datasource native builds)
+    Class<?>[] oracleEntities = {
+        ClientAcronymEntity.class,
+        CutBlockOpenAdminEntity.class,
+        OpenCategoryCodeEntity.class,
+        OrgUnitEntity.class,
+        ResultsElectronicSubmissionEntity.class,
+        ActivityTreatmentUnitEntity.class,
+        SilvicultureCommentEntity.class,
+        ForestCoverEntity.class,
+        OpeningAttachmentEntity.class,
+        OpeningEntity.class
+    };
+
+    // Register all Postgres entities for reflection
+    Class<?>[] postgresEntities = {
+        UserOpeningEntity.class,
+        UserRecentOpeningEntity.class
+    };
+
+    // Register all entity classes with full reflection support
+    for (Class<?> entity : oracleEntities) {
+      hints.reflection().registerType(entity,
+          MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
+          MemberCategory.INVOKE_DECLARED_METHODS,
+          MemberCategory.INVOKE_PUBLIC_METHODS);
+    };
+
+    for (Class<?> entity : postgresEntities) {
+      hints.reflection().registerType(entity,
+          MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
+          MemberCategory.INVOKE_DECLARED_METHODS,
+          MemberCategory.INVOKE_PUBLIC_METHODS);
+    }
   }
 }
