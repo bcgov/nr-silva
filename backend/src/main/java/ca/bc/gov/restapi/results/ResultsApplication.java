c5f9c93816fe99b3c9842d469dbd1cc92a0acd36
Craig Yu <craig.yu93@gmail.com>
2026-01-05 13:17:56 -0800

    Refactor component and entity scanning configuration
    Replaces multiple base packages in @ComponentScan with a single package and introduces @EntityScan for entity packages. This improves clarity and aligns with Spring Boot best practices for scanning components and entities.

--- backend/src/main/java/ca/bc/gov/restapi/results/ResultsApplication.java
+++ backend/src/main/java/ca/bc/gov/restapi/results/ResultsApplication.java
@@ -3,18 +3,18 @@ package ca.bc.gov.restapi.results;
 import ca.bc.gov.restapi.results.config.NativeRuntimeHints;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportRuntimeHints;
 
 /** Entry point for the RESULTS REST API application. */
 @SpringBootApplication
 @ImportRuntimeHints(NativeRuntimeHints.class)
-@ComponentScan(
-    basePackages = {
-      "ca.bc.gov.restapi.results",
-      "ca.bc.gov.restapi.results.oracle.entity",
-      "ca.bc.gov.restapi.results.postgres.entity"
-    })
+@EntityScan(basePackages = {
+    "ca.bc.gov.restapi.results.oracle.entity",
+    "ca.bc.gov.restapi.results.postgres.entity"
+})
+@ComponentScan(basePackages = "ca.bc.gov.restapi.results")
 public class ResultsApplication {
 
   public static void main(String[] args) {
