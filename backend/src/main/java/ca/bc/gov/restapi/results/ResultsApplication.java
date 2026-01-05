8cef9b66119199c049d65938ac46810433f0feef
Craig Yu <craig.yu93@gmail.com>
2026-01-05 14:59:49 -0800

    Remove unused @EntityScan annotation
    The @EntityScan annotation was removed from ResultsApplication as it is no longer needed. This simplifies the application configuration.

--- backend/src/main/java/ca/bc/gov/restapi/results/ResultsApplication.java
+++ backend/src/main/java/ca/bc/gov/restapi/results/ResultsApplication.java
@@ -3,17 +3,12 @@ package ca.bc.gov.restapi.results;
 import ca.bc.gov.restapi.results.config.NativeRuntimeHints;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportRuntimeHints;
 
 /** Entry point for the RESULTS REST API application. */
 @SpringBootApplication
 @ImportRuntimeHints(NativeRuntimeHints.class)
-@EntityScan(basePackages = {
-    "ca.bc.gov.restapi.results.oracle.entity",
-    "ca.bc.gov.restapi.results.postgres.entity"
-})
 @ComponentScan(basePackages = "ca.bc.gov.restapi.results")
 public class ResultsApplication {
 
