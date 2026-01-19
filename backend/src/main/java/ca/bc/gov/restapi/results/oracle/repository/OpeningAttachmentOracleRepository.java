7d25e8f5785ab8adf1c03301cd11b3ac9006a476
Shayan Khorsandi <shayan.khorsandi@gov.bc.ca>
2026-01-19 15:21:17 -0800

    feat(#1024): add @Repository annotation to OpeningAttachmentOracleRepository
    Annotated OpeningAttachmentOracleRepository with @Repository to enable component scanning and exception translation in Spring.

--- backend/src/main/java/ca/bc/gov/restapi/results/oracle/repository/OpeningAttachmentOracleRepository.java
+++ backend/src/main/java/ca/bc/gov/restapi/results/oracle/repository/OpeningAttachmentOracleRepository.java
@@ -9,7 +9,9 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OpeningAttachmentOracleRepository extends JpaRepository<OpeningAttachmentEntity, Long>,
     OpeningAttachmentRepository {
 
