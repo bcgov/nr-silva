3d99eaffc0e42b8d59bb0a674ca5de9bb89305c6
Shayan Khorsandi <shayan.khorsandi@gov.bc.ca>
2026-01-19 15:13:48 -0800

    feat(#1024): add @Repository annotation to activity treatment unit Oracle repository
    Annotated ActivityTreatmentUnitOracleRepository with @Repository to enable Spring's component scanning and exception translation for this interface.

--- backend/src/main/java/ca/bc/gov/restapi/results/oracle/repository/ActivityTreatmentUnitOracleRepository.java
+++ backend/src/main/java/ca/bc/gov/restapi/results/oracle/repository/ActivityTreatmentUnitOracleRepository.java
@@ -16,7 +16,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ActivityTreatmentUnitOracleRepository
     extends JpaRepository<ActivityTreatmentUnitEntity, Long>, ActivityTreatmentUnitRepository {
 
