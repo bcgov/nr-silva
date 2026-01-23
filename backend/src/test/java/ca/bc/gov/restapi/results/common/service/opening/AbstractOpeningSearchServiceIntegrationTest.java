1d6b594bf5ab0075af04cd1709c5c3c896cca418
Shayan Khorsandi <shayan.khorsandi@gov.bc.ca>
2026-01-23 11:30:07 -0800

    test(#1024): add abstract integration and unit tests for service and endpoint layers
    Introduces a suite of abstract integration and unit test classes for various service and endpoint layers, including Opening, OrgUnit, CutBlock, CategoryCode, OpeningTrends, OpeningDetails, and ForestCoverHistory services. These tests provide reusable contract-based test structures to ensure consistent behavior and facilitate easier implementation of concrete test cases.

---
+++ backend/src/test/java/ca/bc/gov/restapi/results/common/service/opening/AbstractOpeningSearchServiceIntegrationTest.java
@@ -0,0 +1,140 @@
+package ca.bc.gov.restapi.results.common.service.opening;
+
+import ca.bc.gov.restapi.results.common.dto.opening.OpeningSearchFiltersDto;
+import ca.bc.gov.restapi.results.common.dto.opening.OpeningSearchResponseDto;
+import ca.bc.gov.restapi.results.common.enums.OpeningCategoryEnum;
+import ca.bc.gov.restapi.results.common.enums.OpeningStatusEnum;
+import ca.bc.gov.restapi.results.common.exception.MaxPageSizeException;
+import ca.bc.gov.restapi.results.common.service.OpeningSearchService;
+import ca.bc.gov.restapi.results.extensions.AbstractTestContainerIntegrationTest;
+import ca.bc.gov.restapi.results.extensions.WiremockLogNotifier;
+import ca.bc.gov.restapi.results.extensions.WithMockJwt;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.List;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+
+@DisplayName("Integrated Test | Opening Search Service | Contract")
+@WithMockJwt(value = "ttester")
+public abstract class AbstractOpeningSearchServiceIntegrationTest<T extends OpeningSearchService> extends
+    AbstractTestContainerIntegrationTest {
+
+  @Autowired
+  protected OpeningSearchService openingSearchService;
+
+  @RegisterExtension
+  static WireMockExtension clientApiStub =
+      WireMockExtension.newInstance()
+          .options(
+              wireMockConfig()
+                  .port(10000)
+                  .notifier(new WiremockLogNotifier())
+                  .asynchronousResponseEnabled(true)
+                  .stubRequestLoggingDisabled(false))
+          .configureStaticDsl(true)
+          .build();
+
+
+  @Test
+  @DisplayName("Opening search file id happy path should succeed")
+  void openingSearch_fileId_shouldSucceed() {
+
+    Page<OpeningSearchResponseDto> result =
+        openingSearchService.openingSearch(
+            new OpeningSearchFiltersDto(
+                null, null, null, null, null,
+                null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+                "101017"),
+            PageRequest.of(0, 10));
+
+    Assertions.assertNotNull(result);
+    Assertions.assertEquals(0, result.getPageable().getPageNumber());
+    Assertions.assertEquals(10, result.getPageable().getPageSize());
+    Assertions.assertEquals(1, result.getTotalPages());
+    Assertions.assertEquals(1, result.getContent().size());
+    Assertions.assertEquals(101017, result.getContent().get(0).getOpeningId());
+    Assertions.assertEquals(" 514", result.getContent().get(0).getOpeningNumber());
+    Assertions.assertEquals(OpeningCategoryEnum.FTML, result.getContent().get(0).getCategory());
+    Assertions.assertEquals(OpeningStatusEnum.FG, result.getContent().get(0).getStatus());
+    Assertions.assertEquals("12K", result.getContent().get(0).getCuttingPermitId());
+  }
+
+  @Test
+  @DisplayName("Opening search org unit happy path should succeed")
+  void openingSearch_orgUnit_shouldSucceed() {
+
+    Page<OpeningSearchResponseDto> result =
+        openingSearchService.openingSearch(
+            new OpeningSearchFiltersDto(
+                List.of("DAS"),
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null),
+            PageRequest.of(0, 10));
+
+    Assertions.assertNotNull(result);
+    Assertions.assertEquals(0, result.getPageable().getPageNumber());
+    Assertions.assertEquals(10, result.getPageable().getPageSize());
+    Assertions.assertEquals(1, result.getTotalPages());
+    Assertions.assertEquals(9, result.getContent().size());
+    Assertions.assertEquals(1796497, result.getContent().get(0).getOpeningId());
+    Assertions.assertNull(result.getContent().get(0).getOpeningNumber());
+  }
+
+  @Test
+  @DisplayName("Opening search no records found should succeed")
+  void openingSearch_noRecordsFound_shouldSucceed() {
+    Page<OpeningSearchResponseDto> result =
+        openingSearchService.openingSearch(
+            new OpeningSearchFiltersDto(
+                null, null, null, null, null, // myOpeningsUserId
+                null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+                "ABCD"),
+            PageRequest.of(0, 10));
+
+    Assertions.assertNotNull(result);
+    Assertions.assertEquals(0, result.getPageable().getPageNumber());
+    Assertions.assertEquals(10, result.getPageable().getPageSize());
+    Assertions.assertEquals(0, result.getTotalPages());
+    Assertions.assertTrue(result.getContent().isEmpty());
+    Assertions.assertFalse(result.hasNext());
+  }
+
+  @Test
+  @DisplayName("Opening search max page exception should fail")
+  void openingSearch_maxPageException_shouldFail() {
+    OpeningSearchFiltersDto filterDto =
+        new OpeningSearchFiltersDto(
+            null, null, null, null, null, // myOpeningsUserId
+            null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+            "FTML");
+    PageRequest pagination = PageRequest.of(0, 2999);
+    Assertions.assertThrows(
+        MaxPageSizeException.class,
+        () -> openingSearchService.openingSearch(filterDto, pagination));
+  }
+}
