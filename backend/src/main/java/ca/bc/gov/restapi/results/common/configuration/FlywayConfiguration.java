b5c28d308fbc20c2d84c1d4fd63d59a08ac1e6bb
Shayan Khorsandi <shayan.khorsandi@gov.bc.ca>
2026-02-25 19:24:59 -0800

    feat: add debug log

--- backend/src/main/java/ca/bc/gov/restapi/results/common/configuration/FlywayConfiguration.java
+++ backend/src/main/java/ca/bc/gov/restapi/results/common/configuration/FlywayConfiguration.java
@@ -1,11 +1,13 @@
 package ca.bc.gov.restapi.results.common.configuration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.flyway.FlywayConfigurationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
 @Configuration
+@Slf4j
 public class FlywayConfiguration {
 
   @Bean
@@ -13,7 +15,7 @@ public class FlywayConfiguration {
     return configuration -> {
       String primaryDatabase = env.getProperty("server.primary-db",
           env.getProperty("PRIMARY_DB", "oracle"));
-       
+      log.info("Configuring Flyway for primary database: {}", primaryDatabase);
       if (primaryDatabase.equals("oracle")) {
         configuration.locations("classpath:db/migration");
       } else if (primaryDatabase.equals("postgres")) {
