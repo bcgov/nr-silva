29ced52bc8e2a042865f57c2c4ccafe40f080f92
Shayan Khorsandi <shayan.khorsandi@gov.bc.ca>
2026-03-02 11:51:59 -0800

    feat: set Flyway schema to 'silva'
    Configure Flyway to use the 'silva' schema by adding schemas() and defaultSchema() in FlywayConfiguration and setting spring.flyway.schemas / spring.flyway.default-schema in application.yml. This ensures migrations are applied to the silva schema and that it is used as the default schema for DB operations.

--- backend/src/main/java/ca/bc/gov/restapi/results/common/configuration/FlywayConfiguration.java
+++ backend/src/main/java/ca/bc/gov/restapi/results/common/configuration/FlywayConfiguration.java
@@ -51,6 +51,8 @@ public class FlywayConfiguration {
       }
 
       configuration
+          .schemas("silva")
+          .defaultSchema("silva")
           .locations(locations.toArray(String[]::new))
           .resourceProvider(new NativeImageResourceProvider(context, locations));
     };
