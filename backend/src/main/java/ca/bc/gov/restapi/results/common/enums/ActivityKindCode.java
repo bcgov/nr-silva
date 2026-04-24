8ae8891a8eef7f7aa497de57afbe0c1d23c5674c
Craig Yu <craig.yu93@gmail.com>
2026-04-24 16:08:56 -0700

    feat: Add enums for comment location and activity kind
    Introduce two new enums in ca.bc.gov.restapi.results.common.enums:
    
    - CommentLocationCode: enumerates silviculture comment attachment targets (STANDARDS_UNIT, OPENING, MILESTONE, ACTIVITIES, FOREST_COVER).
    - ActivityKindCode: distinguishes whether an ACTIVITIES comment belongs to an ACTIVITY or a DISTURBANCE ATU.
    
    These types provide a typed model for comment metadata and will be used when persisting or validating comment-related data.

---
+++ backend/src/main/java/ca/bc/gov/restapi/results/common/enums/ActivityKindCode.java
@@ -0,0 +1,7 @@
+package ca.bc.gov.restapi.results.common.enums;
+
+/** Distinguishes whether an ACTIVITIES comment belongs to an activity or disturbance ATU. */
+public enum ActivityKindCode {
+  ACTIVITY,
+  DISTURBANCE
+}
