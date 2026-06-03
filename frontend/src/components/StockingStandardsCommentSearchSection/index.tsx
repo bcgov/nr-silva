641e4126ff5dbbc449c9bbdbb7553d06368862e7
Craig Yu <craig.yu93@gmail.com>
2026-06-03 11:11:55 -0700

    fix: stash

--- frontend/src/components/StockingStandardsCommentSearchSection/index.tsx
+++ frontend/src/components/StockingStandardsCommentSearchSection/index.tsx
@@ -1,4 +1,9 @@
+import API from "@/services/API";
+
 const StockingStandardsCommentSearchSection = () => {
+  const endpointToUse = API.SearchEndpointService.stockingStandardsCommentSearch
+
+
   return (
     <div>Stocking Standards Comment Search Section</div>
   );
